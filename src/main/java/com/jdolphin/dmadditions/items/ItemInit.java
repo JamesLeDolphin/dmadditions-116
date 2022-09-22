2ef31858cf750a3aa3d10b78525b34ac1b779b90
Torchwood-1 <jacobroberts@virginmedia.com>
2022-09-22 19:18:44 +0100

    the magic gold key is back

--- src/main/java/com/jdolphin/dmadditions/items/ItemInit.java
+++ src/main/java/com/jdolphin/dmadditions/items/ItemInit.java
@@ -1,24 +1,21 @@
 package com.jdolphin.dmadditions.items;
 
-import com.jdolphin.dmadditions.DmAdditions;
+
 import com.swdteam.common.RegistryHandler;
-import com.swdteam.common.init.DMItemTiers;
-import com.swdteam.common.init.DMItems;
-import com.swdteam.common.init.DMProjectiles;
-import com.swdteam.common.init.DMSoundEvents;
+import com.swdteam.common.init.*;
 import com.swdteam.common.item.LasergunItem;
+import com.swdteam.common.item.TardisKeyItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
-import net.minecraftforge.eventbus.api.IEventBus;
+
 import net.minecraftforge.fml.RegistryObject;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.ForgeRegistry;
+
 
 public class ItemInit {
 
 
     public static RegistryObject<Item> PISTOL = RegistryHandler.ITEMS.register("pistol", () -> new LasergunItem(DMItemTiers.DALEK_GUNSTICK, 0.0F, DMProjectiles.GREEN_LASER, DMSoundEvents.ENTITY_DALEK_GUNSTICK_CHARGE, DMSoundEvents.ENTITY_DALEK_GUNSTICK_SHOOT, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
 
+    public static RegistryObject<Item> TARDIS_GOLD_KEY = RegistryHandler.ITEMS.register("tardis_gold_key", () -> new TardisKeyItem((new Item.Properties()).tab(DMTabs.DM_TARDIS), ""));
 
 }
