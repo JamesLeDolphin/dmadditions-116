a70d2de68fd21446648e43da65b84277e45bd6fe
Torchwood-1 <jacobroberts@virginmedia.com>
2022-09-22 20:24:55 +0100

    bug and sam helped me do something cool, they got the magic touch

--- src/main/java/com/jdolphin/dmadditions/items/ItemInit.java
+++ src/main/java/com/jdolphin/dmadditions/items/ItemInit.java
@@ -14,8 +14,11 @@ import net.minecraftforge.fml.RegistryObject;
 public class ItemInit {
 
 
-    public static RegistryObject<Item> PISTOL = RegistryHandler.ITEMS.register("pistol", () -> new LasergunItem(DMItemTiers.DALEK_GUNSTICK, 0.0F, DMProjectiles.GREEN_LASER, DMSoundEvents.ENTITY_DALEK_GUNSTICK_CHARGE, DMSoundEvents.ENTITY_DALEK_GUNSTICK_SHOOT, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
+    public static RegistryObject<Item> PISTOL = RegistryHandler.ITEMS.register("pistol", ()
+            -> new LasergunItem(DMItemTiers.DALEK_GUNSTICK, 0.0F, DMProjectiles.GREEN_LASER, DMSoundEvents.ENTITY_DALEK_GUNSTICK_CHARGE,
+            DMSoundEvents.ENTITY_DALEK_GUNSTICK_SHOOT, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
 
-    public static RegistryObject<Item> TARDIS_GOLD_KEY = RegistryHandler.ITEMS.register("tardis_gold_key", () -> new TardisKeyItem((new Item.Properties()).tab(DMTabs.DM_TARDIS), ""));
+    public static RegistryObject<Item> TARDIS_GOLD_KEY = RegistryHandler.ITEMS.register("tardis_gold_key", ()
+            -> new TardisGoldKeyItem((new Item.Properties()).tab(DMTabs.DM_TARDIS), ""));
 
 }
