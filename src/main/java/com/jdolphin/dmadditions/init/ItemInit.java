f1bf0ad9589cbfbbf06bcac6fa8783c35fcf484f
Torchwood-1 <jacobroberts@virginmedia.com>
2022-09-25 13:51:32 +0100

    fixed a weird error

--- src/main/java/com/jdolphin/dmadditions/init/ItemInit.java
+++ src/main/java/com/jdolphin/dmadditions/init/ItemInit.java
@@ -16,7 +16,7 @@ public class ItemInit {
     public static RegistryObject<Item> DINO_NUGGETS;
 
     public static RegistryObject<Item> PISTOL = RegistryHandler.ITEMS.register("pistol", ()
-            -> new LasergunItem(DMItemTiers.DALEK_GUNSTICK, 0.0F, DMAdditionsProjectiles.PURPLE_LASER, DMSoundEvents.ENTITY_DALEK_GUNSTICK_CHARGE,
+            -> new LasergunItem(DMItemTiers.DALEK_GUNSTICK, 0.15F, DMAdditionsProjectiles.PURPLE_LASER, DMSoundEvents.ENTITY_DALEK_GUNSTICK_CHARGE,
             DMAdditionsSoundEvents.PISTOL_SHOOT, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
 
     public static RegistryObject<Item> TARDIS_GOLD_KEY = RegistryHandler.ITEMS.register("tardis_gold_key", ()
