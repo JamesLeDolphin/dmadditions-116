1c4fc2dd7cf2171637e92579b316f255f1421b65
SamTees <samueltees2016@gmail.com>
2022-10-08 22:29:57 +0100

    fix the lever thing

--- src/main/java/com/jdolphin/dmadditions/blocks/FastReturn.java
+++ src/main/java/com/jdolphin/dmadditions/blocks/FastReturn.java
@@ -1,6 +1,5 @@
 package com.jdolphin.dmadditions.blocks;
 
-import com.swdteam.common.block.tardis.FastReturnLeverBlock;
 import com.swdteam.common.init.DMDimensions;
 import com.swdteam.common.init.DMSoundEvents;
 import com.swdteam.common.init.DMTardis;
@@ -8,81 +7,25 @@ import com.swdteam.common.init.DMTranslationKeys;
 import com.swdteam.common.tardis.TardisData;
 import com.swdteam.common.tardis.data.TardisFlightPool;
 import com.swdteam.util.ChatUtil;
-import net.minecraft.block.*;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.LeverBlock;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.state.BooleanProperty;
-import net.minecraft.state.StateContainer;
-import net.minecraft.state.properties.AttachFace;
-import net.minecraft.state.properties.BlockStateProperties;
-import net.minecraft.util.*;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
-import net.minecraft.util.math.shapes.ISelectionContext;
-import net.minecraft.util.math.shapes.VoxelShape;
-import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 
-import static com.swdteam.common.block.AbstractLeverBlock.PULLED;
-import static net.minecraft.block.HorizontalBlock.FACING;
-import static net.minecraft.block.HorizontalFaceBlock.FACE;
-
-public class FastReturn extends HorizontalFaceBlock {
-    public static final BooleanProperty POWERED = BlockStateProperties.POWERED;
-    protected static final VoxelShape NORTH_AABB = Block.box(5.0D, 4.0D, 10.0D, 11.0D, 12.0D, 16.0D);
-    protected static final VoxelShape SOUTH_AABB = Block.box(5.0D, 4.0D, 0.0D, 11.0D, 12.0D, 6.0D);
-    protected static final VoxelShape WEST_AABB = Block.box(10.0D, 4.0D, 5.0D, 16.0D, 12.0D, 11.0D);
-    protected static final VoxelShape EAST_AABB = Block.box(0.0D, 4.0D, 5.0D, 6.0D, 12.0D, 11.0D);
-    protected static final VoxelShape UP_AABB_Z = Block.box(5.0D, 0.0D, 4.0D, 11.0D, 6.0D, 12.0D);
-    protected static final VoxelShape UP_AABB_X = Block.box(4.0D, 0.0D, 5.0D, 12.0D, 6.0D, 11.0D);
-    protected static final VoxelShape DOWN_AABB_Z = Block.box(5.0D, 10.0D, 4.0D, 11.0D, 16.0D, 12.0D);
-    protected static final VoxelShape DOWN_AABB_X = Block.box(4.0D, 10.0D, 5.0D, 12.0D, 16.0D, 11.0D);
-
-    public FastReturn(AbstractBlock.Properties b) {
-        super(b);
-        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH).setValue(POWERED, Boolean.FALSE).setValue(FACE, AttachFace.WALL));
-    }
-
-    public VoxelShape getShape(BlockState p_220053_1_, IBlockReader p_220053_2_, BlockPos p_220053_3_, ISelectionContext p_220053_4_) {
-        switch((AttachFace)p_220053_1_.getValue(FACE)) {
-            case FLOOR:
-                switch(p_220053_1_.getValue(FACING).getAxis()) {
-                    case X:
-                        return UP_AABB_X;
-                    case Z:
-                    default:
-                        return UP_AABB_Z;
-                }
-            case WALL:
-                switch((Direction)p_220053_1_.getValue(FACING)) {
-                    case EAST:
-                        return EAST_AABB;
-                    case WEST:
-                        return WEST_AABB;
-                    case SOUTH:
-                        return SOUTH_AABB;
-                    case NORTH:
-                    default:
-                        return NORTH_AABB;
-                }
-            case CEILING:
-            default:
-                switch(p_220053_1_.getValue(FACING).getAxis()) {
-                    case X:
-                        return DOWN_AABB_X;
-                    case Z:
-                    default:
-                        return DOWN_AABB_Z;
-                }
-        }
-    }
-
-    protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> FACING) {
-        super.createBlockStateDefinition(FACING);
+public class FastReturn extends LeverBlock {
+    public FastReturn(Properties properties) {
+        super(properties);
     }
 
     public ActionResultType use(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult p_225533_6_) {
         if (handIn == Hand.MAIN_HAND) {
-            worldIn.setBlockAndUpdate(pos, (BlockState)state.setValue(PULLED, !(Boolean)state.getValue(PULLED)));
+            worldIn.setBlockAndUpdate(pos, (BlockState)state.setValue(POWERED, !(Boolean)state.getValue(POWERED)));
             worldIn.playSound((PlayerEntity)null, (double)pos.getX(), (double)pos.getY(), (double)pos.getZ(), (SoundEvent) DMSoundEvents.TARDIS_CONTROLS_LEVER.get(), SoundCategory.BLOCKS, 1.0F, 1.0F);
             if (worldIn.dimension().equals(DMDimensions.TARDIS) && !worldIn.isClientSide) {
                 TardisData data = DMTardis.getTardisFromInteriorPos(pos);
--- src/main/java/com/jdolphin/dmadditions/init/DMAdditionsBlocks.java
+++ src/main/java/com/jdolphin/dmadditions/init/DMAdditionsBlocks.java
@@ -1,26 +1,16 @@
 package com.jdolphin.dmadditions.init;
 
 import com.jdolphin.dmadditions.blocks.FastReturn;
-import com.swdteam.common.block.tardis.FastReturnLeverBlock;
 import com.swdteam.common.init.DMTabs;
 import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraftforge.fml.RegistryObject;
 
-import static com.swdteam.common.RegistryHandler.BLOCKS;
-import static com.swdteam.common.init.DMBlocks.FAST_RETURN_LEVER;
 import static com.swdteam.common.init.DMBlocks.registerBlock;
 
 public class DMAdditionsBlocks {
-    public static final RegistryObject<Block> FAST_RETURN = BLOCKS.register("fast_return",
-            () -> new FastReturn(Block.Properties.of(Material.STONE)));
-
-
-   /* static {
-        FAST_RETURN = registerBlock(() -> {
-            return new FastReturn(net.minecraft.block.AbstractBlock.Properties.of(Material.STONE).instabreak().noOcclusion().sound(SoundType.STONE));
-        }, "fast_return", DMTabs.DM_TARDIS);
-    }*/
+	public static final RegistryObject<Block> FAST_RETURN = registerBlock(
+			() -> new FastReturn(net.minecraft.block.AbstractBlock.Properties.of(Material.STONE).instabreak().noOcclusion().sound(SoundType.STONE)),
+			"fast_return", DMTabs.DM_TARDIS);
 }
