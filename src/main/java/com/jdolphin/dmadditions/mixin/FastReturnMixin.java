d95edf2464084e5d21028c4b133250a0e4d0ba91
James Dolphin <karl.kallip@gmail.com>
2022-10-11 16:16:52 +0300

    Stuff

--- src/main/java/com/jdolphin/dmadditions/mixin/FastReturnMixin.java
+++ src/main/java/com/jdolphin/dmadditions/mixin/FastReturnMixin.java
@@ -1,58 +1,22 @@
 
 package com.jdolphin.dmadditions.mixin;
 
-import com.swdteam.common.block.tardis.FastReturnLeverBlock;
-import com.swdteam.common.init.DMDimensions;
-import com.swdteam.common.init.DMSoundEvents;
-import com.swdteam.common.init.DMTardis;
-import com.swdteam.common.init.DMTranslationKeys;
-import com.swdteam.common.tardis.TardisData;
-import com.swdteam.common.tardis.data.TardisFlightPool;
-import com.swdteam.util.ChatUtil;
-import net.minecraft.block.BlockState;
+import com.swdteam.common.init.DMBlocks;
 import net.minecraft.block.LeverBlock;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Hand;
-import net.minecraft.util.SoundCategory;
-import net.minecraft.util.SoundEvent;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.BlockRayTraceResult;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(FastReturnLeverBlock.class)
-public abstract class FastReturnMixin extends LeverBlock {
+@Mixin(DMBlocks.class)
+public abstract class FastReturnMixin {
 
-    public FastReturnMixin(Properties properties) {
-        super(properties);
-    }
 
-    @Inject(at = @At("HEAD"), method = "use(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/Hand;Lnet/minecraft/util/math/BlockRayTraceResult;)Lnet/minecraft/util/ActionResultType;"
+    @Inject(at = @At("RETURN"), method = "Lcom/swdteam/common/init/DMBlocks;FAST_RETURN_LEVER:Lnet/minecraftforge/fml/RegistryObject;"
     )
-    private ActionResultType onUse(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult p_225533_6_, CallbackInfoReturnable<ActionResultType> cir) {
+    private void onRegister(CallbackInfoReturnable<Something> cir) {
 
         //Cancel the method at it's head, so it basically does nothing
         cir.cancel();
-        {
-            if (handIn == Hand.MAIN_HAND) {
-                worldIn.setBlockAndUpdate(pos, (BlockState) state.setValue(POWERED, !(Boolean) state.getValue(POWERED)));
-                worldIn.playSound((PlayerEntity) null, (double) pos.getX(), (double) pos.getY(), (double) pos.getZ(), (SoundEvent) DMSoundEvents.TARDIS_CONTROLS_LEVER.get(), SoundCategory.BLOCKS, 1.0F, 1.0F);
-                if (worldIn.dimension().equals(DMDimensions.TARDIS) && !worldIn.isClientSide) {
-                    TardisData data = DMTardis.getTardisFromInteriorPos(pos);
-                    if (data != null && data.getPreviousLocation() != null) {
-                        TardisFlightPool.updateFlight(data, data.getPreviousLocation());
-                    }
-
-                    ChatUtil.sendCompletedMsg(player, DMTranslationKeys.TARDIS_FAST_RETURN_SET, ChatUtil.MessageType.STATUS_BAR);
-                }
-            }
-
-            return ActionResultType.CONSUME;
-        }
 
     }
 }
