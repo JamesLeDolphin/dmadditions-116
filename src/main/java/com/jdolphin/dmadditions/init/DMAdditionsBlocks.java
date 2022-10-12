314abe3e5c02e4b94ec54c4c6baad194941340b2
SamTees <samueltees2016@gmail.com>
2022-10-12 18:13:58 +0100

    fix the lever thing

--- src/main/java/com/jdolphin/dmadditions/init/DMAdditionsBlocks.java
+++ src/main/java/com/jdolphin/dmadditions/init/DMAdditionsBlocks.java
@@ -3,21 +3,31 @@ package com.jdolphin.dmadditions.init;
 import com.jdolphin.dmadditions.blocks.FastReturn;
 import com.jdolphin.dmadditions.blocks.Randomiser;
 import com.swdteam.common.init.DMTabs;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
-import net.minecraft.item.ItemGroup;
 import net.minecraftforge.fml.RegistryObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
 
 import static com.swdteam.common.init.DMBlocks.registerBlock;
 
 public class DMAdditionsBlocks {
-	public static final RegistryObject<Block> FAST_RETURN = registerBlock(
-			() -> new FastReturn(net.minecraft.block.AbstractBlock.Properties.of(Material.STONE).instabreak().noOcclusion().sound(SoundType.STONE)),
-			"fast_return", DMTabs.DM_TARDIS);
+	public static final Map<String, Supplier<Block>> MIXIN_BLOCKS;
 
-	public static final RegistryObject<Block> RANDOMISER = registerBlock(
-			() -> new Randomiser(net.minecraft.block.AbstractBlock.Properties.of(Material.STONE).instabreak().noOcclusion().sound(SoundType.STONE)),
-			"randomizer", DMTabs.DM_TARDIS);
+	static {
+		MIXIN_BLOCKS = new HashMap<>();
+
+		MIXIN_BLOCKS.put("fast_return_lever",
+				() -> new FastReturn(AbstractBlock.Properties.of(Material.STONE).instabreak().noOcclusion().sound(SoundType.STONE)));
+
+	}
+
+//	public static final RegistryObject<Block> RANDOMISER = registerBlock(
+//			() -> new Randomiser(net.minecraft.block.AbstractBlock.Properties.of(Material.STONE).instabreak().noOcclusion().sound(SoundType.STONE)),
+//			"randomizer", DMTabs.DM_TARDIS);
 
 }
---
+++ src/main/java/com/jdolphin/dmadditions/mixin/DMBlocksMixin.java
@@ -0,0 +1,37 @@
+package com.jdolphin.dmadditions.mixin;
+
+import com.swdteam.common.RegistryHandler;
+import com.swdteam.common.init.DMBlocks;
+import com.swdteam.common.item.BaseBlockItem;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.function.Supplier;
+
+import static com.jdolphin.dmadditions.init.DMAdditionsBlocks.MIXIN_BLOCKS;
+
+@Mixin({DMBlocks.class})
+public abstract class DMBlocksMixin {
+
+	@Inject(method = "registerBlock(Ljava/util/function/Supplier;Ljava/lang/String;Lnet/minecraft/item/Item$Properties;Z)Lnet/minecraftforge/fml/RegistryObject;", at = @At("HEAD"), cancellable = true)
+	private static <B extends Block> void registerBlock(Supplier<B> block, String name, Item.Properties properties, boolean needsItem, CallbackInfoReturnable<RegistryObject<Block>> cir) {
+//		System.out.printf("Block is being registered: %s!%n", name);
+		if (MIXIN_BLOCKS.containsKey(name)) {
+			System.out.printf("Mi want chanj dis bluk: %s!%n", name);
+
+			RegistryObject<Block> blockObj = RegistryHandler.BLOCKS.register(name, MIXIN_BLOCKS.get(name));
+			if (needsItem) {
+				RegistryHandler.ITEMS.register(name,
+						() -> new BaseBlockItem(blockObj.get(), properties));
+			}
+			cir.setReturnValue(blockObj);
+			cir.cancel();
+		}
+	}
+
+}
