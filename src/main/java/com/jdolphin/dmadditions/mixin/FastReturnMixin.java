be8bcbc771206cb241fb4d4824373f54c8cb2ef8
James Dolphin <karl.kallip@gmail.com>
2022-10-12 16:39:09 +0300

    Messed around

--- src/main/java/com/jdolphin/dmadditions/mixin/FastReturnMixin.java
+++ src/main/java/com/jdolphin/dmadditions/mixin/FastReturnMixin.java
@@ -1,22 +1,43 @@
-
 package com.jdolphin.dmadditions.mixin;
 
-import com.swdteam.common.init.DMBlocks;
-import net.minecraft.block.LeverBlock;
+import static com.swdteam.common.init.DMBlocks.registerBlock;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(DMBlocks.class)
-public abstract class FastReturnMixin {
+import com.jdolphin.dmadditions.blocks.FastReturn;
+import com.swdteam.common.init.DMBlocks;
+import com.swdteam.common.init.DMTabs;
+
+import net.minecraft.block.LeverBlock;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+
+@Mixin({DMBlocks.class})
+public abstract class FastReturnMixin extends LeverBlock{
 
 
-    @Inject(at = @At("RETURN"), method = "Lcom/swdteam/common/init/DMBlocks;FAST_RETURN_LEVER:Lnet/minecraftforge/fml/RegistryObject;"
+    @SuppressWarnings("unused")
+	private static Object FAST_RETURN_LEVER = DMBlocks.FAST_RETURN_LEVER;
+
+    public FastReturnMixin(Properties p_i48369_1_) {
+		super(p_i48369_1_);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Inject(
+            at = @At(value = "RETURN", ordinal = 239),
+            method = {"Lcom/swdteam/common/init/DMBlocks;FAST_RETURN_LEVER:Lnet/minecraftforge/fml/RegistryObject;"}
     )
-    private void onRegister(CallbackInfoReturnable<Something> cir) {
-
-        //Cancel the method at it's head, so it basically does nothing
-        cir.cancel();
-
+    private static void onSomething(CallbackInfoReturnable<Object> cir) {
+        cir.setReturnValue(
+        FAST_RETURN_LEVER = registerBlock(() -> {
+            return new FastReturn(Properties.of(Material.STONE).instabreak().noOcclusion().sound(SoundType.STONE));
+        }, "fast_return_lever", DMTabs.DM_TARDIS));
     }
+
+
+
 }
