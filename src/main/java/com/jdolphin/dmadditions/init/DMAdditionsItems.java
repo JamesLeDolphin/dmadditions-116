74bf2998048a8ae6efa41c3865c973e1e173f208
SamTees <samueltees2016@gmail.com>
2022-10-12 21:00:21 +0100

    put things in the static thing

--- src/main/java/com/jdolphin/dmadditions/init/DMAdditionsItems.java
+++ src/main/java/com/jdolphin/dmadditions/init/DMAdditionsItems.java
@@ -15,21 +15,21 @@ public class DMAdditionsItems {
 
 	public static RegistryObject<Item> DINO_NUGGETS;
 	public static RegistryObject<Item> DINO_NUGGETS_CUSTARD;
-
-	public static RegistryObject<Item> PISTOL = RegistryHandler.ITEMS.register("pistol", ()
-			-> new LasergunItem(DMItemTiers.DALEK_GUNSTICK, 0.15F, DMAdditionsProjectiles.PURPLE_LASER, DMSoundEvents.ENTITY_DALEK_GUNSTICK_CHARGE,
-			DMAdditionsSoundEvents.PISTOL_SHOOT, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
-
-	public static RegistryObject<Item> TARDIS_GOLD_KEY = RegistryHandler.ITEMS.register("tardis_gold_key", ()
-			-> new TardisGoldKeyItem((new Item.Properties()).tab(DMTabs.DM_TARDIS), ""));
-
+	public static RegistryObject<Item> PISTOL;
+	public static RegistryObject<Item> TARDIS_GOLD_KEY;
 
 	static {
-		DINO_NUGGETS = RegistryHandler.ITEMS.register("dino_nuggets", () -> {
-			return new FoodItem((new Item.Properties()).food(DMAdditionsFoods.DINO_NUGGETS).tab(ItemGroup.TAB_FOOD));
-		});
-		DINO_NUGGETS_CUSTARD = RegistryHandler.ITEMS.register("dino_nuggets_custard", () -> {
-			return new FoodItem((new Item.Properties()).food(DMAdditionsFoods.DINO_NUGGETS_CUSTARD).tab(ItemGroup.TAB_FOOD));
-		});
+		PISTOL = RegistryHandler.ITEMS.register("pistol",
+				() -> new LasergunItem(DMItemTiers.DALEK_GUNSTICK, 0.15F, DMAdditionsProjectiles.PURPLE_LASER, DMSoundEvents.ENTITY_DALEK_GUNSTICK_CHARGE,
+						DMAdditionsSoundEvents.PISTOL_SHOOT, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
+
+		TARDIS_GOLD_KEY = RegistryHandler.ITEMS.register("tardis_gold_key",
+				() -> new TardisGoldKeyItem((new Item.Properties()).tab(DMTabs.DM_TARDIS), ""));
+
+		DINO_NUGGETS = RegistryHandler.ITEMS.register("dino_nuggets",
+				() -> new FoodItem((new Item.Properties()).food(DMAdditionsFoods.DINO_NUGGETS).tab(ItemGroup.TAB_FOOD)));
+
+		DINO_NUGGETS_CUSTARD = RegistryHandler.ITEMS.register("dino_nuggets_custard",
+				() -> new FoodItem((new Item.Properties()).food(DMAdditionsFoods.DINO_NUGGETS_CUSTARD).tab(ItemGroup.TAB_FOOD)));
 	}
 }
