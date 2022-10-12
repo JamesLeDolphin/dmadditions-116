154e13e15c5280355842cf834ffbba24db3dca67
SamTees <samueltees2016@gmail.com>
2022-10-12 18:35:36 +0100

    set default face to floor on fast return lever

--- src/main/java/com/jdolphin/dmadditions/blocks/BetterFastReturnLeverBlock.java
+++ src/main/java/com/jdolphin/dmadditions/blocks/BetterFastReturnLeverBlock.java
@@ -10,10 +10,8 @@ import com.swdteam.util.ChatUtil;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.LeverBlock;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Hand;
-import net.minecraft.util.SoundCategory;
-import net.minecraft.util.SoundEvent;
+import net.minecraft.state.properties.AttachFace;
+import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.World;
@@ -21,6 +19,7 @@ import net.minecraft.world.World;
 public class BetterFastReturnLeverBlock extends LeverBlock {
     public BetterFastReturnLeverBlock(Properties properties) {
         super(properties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH).setValue(POWERED, Boolean.FALSE).setValue(FACE, AttachFace.FLOOR));
     }
 
     public ActionResultType use(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult p_225533_6_) {
