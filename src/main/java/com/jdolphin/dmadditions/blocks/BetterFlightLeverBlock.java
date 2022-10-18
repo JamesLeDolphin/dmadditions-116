d16648622397f3441e780481c7f64a5fa654e9f7
Sam <26529248+SamTees@users.noreply.github.com>
2022-10-18 16:33:14 +0100

    Add BetterTardisLeverBlock class

--- src/main/java/com/jdolphin/dmadditions/blocks/BetterFlightLeverBlock.java
+++ src/main/java/com/jdolphin/dmadditions/blocks/BetterFlightLeverBlock.java
@@ -6,12 +6,10 @@ import com.swdteam.common.init.DMTardis;
 import com.swdteam.common.tardis.TardisData;
 import com.swdteam.common.tardis.actions.TardisActionList;
 import com.swdteam.util.ChatUtil;
+
 import net.minecraft.block.BlockState;
-import net.minecraft.block.LeverBlock;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.state.properties.AttachFace;
 import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
@@ -20,10 +18,9 @@ import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 
-public class BetterFlightLeverBlock extends LeverBlock {
+public class BetterFlightLeverBlock extends BetterTardisLeverBlock {
 	public BetterFlightLeverBlock(Properties properties) {
 		super(properties);
-		this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH).setValue(POWERED, Boolean.FALSE).setValue(FACE, AttachFace.FLOOR));
 	}
 
 	public ActionResultType use(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult p_225533_6_) {
@@ -53,14 +50,4 @@ public class BetterFlightLeverBlock extends LeverBlock {
 		return ActionResultType.CONSUME;
 	}
 
-	public void switchLever(BlockState state, World worldIn, BlockPos pos) {
-		worldIn.setBlockAndUpdate(pos, state.setValue(POWERED, !(Boolean) state.getValue(POWERED)));
-		worldIn.playSound(null, pos.getX(), pos.getY(), pos.getZ(), DMSoundEvents.TARDIS_CONTROLS_LEVER.get(), SoundCategory.BLOCKS, 1.0F, 1.0F);
-		this.updateNeighbours(state, worldIn, pos);
-	}
-
-	private void updateNeighbours(BlockState state, World world, BlockPos pos) {
-		world.updateNeighborsAt(pos, this);
-		world.updateNeighborsAt(pos.relative(getConnectedDirection(state).getOpposite()), this);
-	}
 }
