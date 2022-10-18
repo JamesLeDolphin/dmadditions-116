61a7ae57d933ae19a14f3e5617b9e54d5becdad5
Sam <26529248+SamTees@users.noreply.github.com>
2022-10-18 16:55:02 +0100

    Remove unneeded classes for flight lever blocks

--- src/main/java/com/jdolphin/dmadditions/blocks/CopperFlightLever.java
+++
@@ -1,7 +0,0 @@
-package com.jdolphin.dmadditions.blocks;
-
-public class CopperFlightLever extends BetterFlightLeverBlock{
-    public CopperFlightLever(Properties properties) {
-        super(properties);
-    }
-}
--- src/main/java/com/jdolphin/dmadditions/init/DMAdditionsBlocks.java
+++ src/main/java/com/jdolphin/dmadditions/init/DMAdditionsBlocks.java
@@ -1,21 +1,20 @@
 package com.jdolphin.dmadditions.init;
 
-import com.jdolphin.dmadditions.blocks.BetterFastReturnLeverBlock;
-import com.jdolphin.dmadditions.blocks.BetterFlightLeverBlock;
-import com.jdolphin.dmadditions.blocks.CoralFlightLever;
-import com.swdteam.common.init.DMTabs;
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.SoundType;
-import net.minecraft.block.material.Material;
-import net.minecraft.block.Blocks;
-import net.minecraftforge.fml.RegistryObject;
+import static com.swdteam.common.init.DMBlocks.registerBlock;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Supplier;
 
-import static com.swdteam.common.init.DMBlocks.registerBlock;
+import com.jdolphin.dmadditions.blocks.BetterFastReturnLeverBlock;
+import com.jdolphin.dmadditions.blocks.BetterFlightLeverBlock;
+import com.swdteam.common.init.DMTabs;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraftforge.fml.RegistryObject;
 
 public class DMAdditionsBlocks {
 	public static final Map<String, Supplier<Block>> MIXIN_BLOCKS;
@@ -30,12 +29,13 @@ public class DMAdditionsBlocks {
 				() -> new BetterFlightLeverBlock(AbstractBlock.Properties.of(Material.STONE).instabreak().noOcclusion().sound(SoundType.STONE)));
 
 	}
+
 	public static final RegistryObject<Block> CORAL_FLIGHT_LEVER = registerBlock(
-			() -> new CoralFlightLever(net.minecraft.block.AbstractBlock.Properties.of(Material.STONE).instabreak().noOcclusion().sound(SoundType.STONE)),
+			() -> new BetterFlightLeverBlock(net.minecraft.block.AbstractBlock.Properties.of(Material.STONE).instabreak().noOcclusion().sound(SoundType.STONE)),
 			"coral_flight_lever", DMTabs.DM_TARDIS);
 
 	public static final RegistryObject<Block> COPPER_FLIGHT_LEVER = registerBlock(
-		() -> new CoralFlightLever(net.minecraft.block.AbstractBlock.Properties.of(Material.STONE).instabreak().noOcclusion().sound(SoundType.STONE)),
+		() -> new BetterFlightLeverBlock(net.minecraft.block.AbstractBlock.Properties.of(Material.STONE).instabreak().noOcclusion().sound(SoundType.STONE)),
 		"copper_flight_lever", DMTabs.DM_TARDIS);
 
 //	public static final RegistryObject<Block> RANDOMISER = registerBlock(
