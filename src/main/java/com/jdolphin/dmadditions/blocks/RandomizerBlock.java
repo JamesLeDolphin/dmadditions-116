7621a10d2f8871b8fc16a7a07bb076f8380e7c95
SamTees <samueltees2016@gmail.com>
2022-10-21 13:54:18 +0100

    Fix randomizer model

--- src/main/java/com/jdolphin/dmadditions/blocks/RandomizerBlock.java
+++ src/main/java/com/jdolphin/dmadditions/blocks/RandomizerBlock.java
@@ -7,29 +7,67 @@ import com.swdteam.common.tardis.TardisFlightData;
 import com.swdteam.common.tardis.data.TardisFlightPool;
 import com.swdteam.util.ChatUtil;
 import com.swdteam.util.ChatUtil.MessageType;
-
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalBlock;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Hand;
-import net.minecraft.util.RegistryKey;
-import net.minecraft.util.ResourceLocation;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.StateContainer;
+import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 import net.minecraft.world.border.WorldBorder;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.fml.server.ServerLifecycleHooks;
 
-public class RandomizerBlock extends Block {
+public class RandomizerBlock extends HorizontalBlock {
 	private String dimensionKey;
+
+	protected static final VoxelShape SHAPE_N = VoxelShapes.or(Block.box(0.0D, 0.0D, 0.0D, 16.0D, 2.0D, 8.0D), Block.box(0.0D, 0.0D, 8.0D, 16.0D, 8.0D, 16.0D));
+	protected static final VoxelShape SHAPE_E = VoxelShapes.or(Block.box(8.0D, 0.0D, 0.0D, 16.0D, 2.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 8.0D, 8.0D, 16.0D));
+	protected static final VoxelShape SHAPE_S = VoxelShapes.or(Block.box(0.0D, 0.0D, 8.0D, 16.0D, 2.0D, 16.0D), Block.box(0.0D, 0.0D, 0.0D, 16.0D, 8.0D, 8.0D));
+	protected static final VoxelShape SHAPE_W = VoxelShapes.or(Block.box(0.0D, 0.0D, 0.0D, 8.0D, 2.0D, 16.0D), Block.box(8.0D, 0.0D, 0.0D, 16.0D, 8.0D, 16.0D));
 
 	public RandomizerBlock(AbstractBlock.Properties properties) {
 		super(properties);
+
+		this.registerDefaultState(this.stateDefinition.any()
+			.setValue(FACING, Direction.NORTH)
+		);
+	}
+
+	public BlockState getStateForPlacement(BlockItemUseContext context) {
+		return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+	}
+
+	@Override
+	public VoxelShape getShape(BlockState state, IBlockReader reader, BlockPos pos, ISelectionContext selectionContext) {
+		Direction facing = state.getValue(FACING);
+		switch (facing) {
+			case NORTH:
+			default:
+				return SHAPE_N;
+			case EAST:
+				return SHAPE_E;
+			case SOUTH:
+				return SHAPE_S;
+			case WEST:
+				return SHAPE_W;
+		}
+	}
+
+	@Override
+	protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> builder) {
+		super.createBlockStateDefinition(builder);
+		builder.add(FACING);
 	}
 
 	public RegistryKey<World> dimensionWorldKey() {
@@ -37,7 +75,7 @@ public class RandomizerBlock extends Block {
 	}
 
 	public ActionResultType use(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn,
-			BlockRayTraceResult rayTraceResult) {
+	                            BlockRayTraceResult rayTraceResult) {
 		if (worldIn.isClientSide)
 			return ActionResultType.PASS;
 
@@ -75,8 +113,8 @@ public class RandomizerBlock extends Block {
 			TardisFlightPool.updateFlight(tardis, new Location(newPos, flight.dimensionWorldKey()));
 
 			ChatUtil.sendCompletedMsg(player,
-					new TranslationTextComponent("notice.dalekmod.tardis.randomizer_set", newPos.getX(), newPos.getZ()),
-					MessageType.STATUS_BAR);
+				new TranslationTextComponent("notice.dalekmod.tardis.randomizer_set", newPos.getX(), newPos.getZ()),
+				MessageType.STATUS_BAR);
 		}
 		return ActionResultType.SUCCESS;
 	}
