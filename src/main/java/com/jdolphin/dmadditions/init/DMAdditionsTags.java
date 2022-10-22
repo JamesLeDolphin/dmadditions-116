09caec0144fc99046044419ff39caad778a953d4
SamTees <26529248+SamTees@users.noreply.github.com>
2022-10-22 23:08:17 +0100

    Add block tags for roundel containers

---
+++ src/main/java/com/jdolphin/dmadditions/init/DMAdditionsTags.java
@@ -0,0 +1,28 @@
+package com.jdolphin.dmadditions.init;
+
+import net.minecraft.block.Block;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.Tags;
+
+public class DMAdditionsTags {
+	public static class Blocks {
+
+		public static final Tags.IOptionalNamedTag<Block> ROUNDEL_CONTAINERS = createTag("roundel_containers");
+
+
+		private static Tags.IOptionalNamedTag<Block> createTag(String name, Boolean itemTag) {
+			if (itemTag) {
+				ItemTags.createOptional(new ResourceLocation("dalekmod", name));
+			}
+
+			return BlockTags.createOptional(new ResourceLocation("dalekmod", name));
+		}
+
+		private static Tags.IOptionalNamedTag<Block> createTag(String name) {
+			return createTag(name, true);
+		}
+	}
+
+}
