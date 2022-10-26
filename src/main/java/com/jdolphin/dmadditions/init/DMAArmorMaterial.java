9c5f578298357783567b92861040c62856d63bd7
SamTees <26529248+SamTees@users.noreply.github.com>
2022-10-27 00:35:02 +0100

    Add armor

---
+++ src/main/java/com/jdolphin/dmadditions/init/DMAArmorMaterial.java
@@ -0,0 +1,84 @@
+package com.jdolphin.dmadditions.init;
+
+import com.swdteam.common.init.DMItems;
+import com.swdteam.main.DalekMod;
+import mcp.MethodsReturnNonnullByDefault;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.LazyValue;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.SoundEvents;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import java.util.function.Supplier;
+
+@MethodsReturnNonnullByDefault
+public enum DMAArmorMaterial implements IArmorMaterial {
+	STEEL("steel", 20, new int[]{2, 5, 6, 2}, 9, SoundEvents.ARMOR_EQUIP_IRON, 0.0F, 0.0F,
+		() -> Ingredient.of(DMItems.STEEL_INGOT.get())),
+	DALEKANIUM("dalekanium", 15, new int[]{2, 5, 6, 2}, 9, SoundEvents.ARMOR_EQUIP_IRON, 0.0F, 0.0F,
+		() -> Ingredient.of(DMItems.DALEKANIUM_INGOT.get())),
+	REFINED_DALEKANIUM("refined_dalekanium", 20, new int[]{2, 5, 6, 2}, 9, SoundEvents.ARMOR_EQUIP_IRON, 0.0F, 0.0F,
+		() -> Ingredient.of(DMItems.DALEKANIUM_INGOT.get())),
+	PURE_DALEKANIUM("pure_dalekanium", 25, new int[]{2, 5, 6, 2}, 10, SoundEvents.ARMOR_EQUIP_IRON, 0.1F, 0.0F,
+		() -> Ingredient.of(DMItems.DALEKANIUM_INGOT.get())),
+	METALERT("metalert", 40, new int[]{3, 6, 8, 3}, 15, SoundEvents.ARMOR_EQUIP_NETHERITE, 3.0F, 0.1F,
+		() -> Ingredient.of(DMItems.METALERT.get()));
+
+	private static final int[] HEALTH_PER_SLOT = new int[]{13, 15, 16, 11};
+	private final String name;
+	private final int durabilityMultiplier;
+	private final int[] slotProtections;
+	private final int enchantmentValue;
+	private final SoundEvent sound;
+	private final float toughness;
+	private final float knockbackResistance;
+	private final LazyValue<Ingredient> repairIngredient;
+
+	DMAArmorMaterial(String pName, int pDurabilityMultiplier, int[] pSlotProtections, int pEnchantmentValue, SoundEvent pSound, float pToughness, float pKnockbackResistance, Supplier<Ingredient> pRepairIngredient) {
+		this.name = pName;
+		this.durabilityMultiplier = pDurabilityMultiplier;
+		this.slotProtections = pSlotProtections;
+		this.enchantmentValue = pEnchantmentValue;
+		this.sound = pSound;
+		this.toughness = pToughness;
+		this.knockbackResistance = pKnockbackResistance;
+		this.repairIngredient = new LazyValue<>(pRepairIngredient);
+	}
+
+	public int getDurabilityForSlot(EquipmentSlotType p_200896_1_) {
+		return HEALTH_PER_SLOT[p_200896_1_.getIndex()] * this.durabilityMultiplier;
+	}
+
+	public int getDefenseForSlot(EquipmentSlotType p_200902_1_) {
+		return this.slotProtections[p_200902_1_.getIndex()];
+	}
+
+	public int getEnchantmentValue() {
+		return this.enchantmentValue;
+	}
+
+	public SoundEvent getEquipSound() {
+		return this.sound;
+	}
+
+	public Ingredient getRepairIngredient() {
+		return this.repairIngredient.get();
+	}
+
+	@OnlyIn(Dist.CLIENT)
+	public String getName() {
+		return new ResourceLocation(DalekMod.MODID, this.name).toString();
+	}
+
+	public float getToughness() {
+		return this.toughness;
+	}
+
+	public float getKnockbackResistance() {
+		return this.knockbackResistance;
+	}
+}
--- src/main/java/com/jdolphin/dmadditions/init/DMAItems.java
+++ src/main/java/com/jdolphin/dmadditions/init/DMAItems.java
@@ -7,6 +7,8 @@ import com.swdteam.common.init.DMSoundEvents;
 import com.swdteam.common.init.DMTabs;
 import com.swdteam.common.item.FoodItem;
 import com.swdteam.common.item.LasergunItem;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraftforge.fml.RegistryObject;
@@ -18,6 +20,31 @@ public class DMAItems {
 	public static RegistryObject<Item> DINO_NUGGETS_CUSTARD;
 	public static RegistryObject<Item> PISTOL;
 	public static RegistryObject<Item> TARDIS_GOLD_KEY;
+
+	public static RegistryObject<Item> STEEL_HELMET;
+	public static RegistryObject<Item> STEEL_CHESTPLATE;
+	public static RegistryObject<Item> STEEL_LEGGINGS;
+	public static RegistryObject<Item> STEEL_BOOTS;
+
+	public static RegistryObject<Item> DALEKANIUM_HELMET;
+	public static RegistryObject<Item> DALEKANIUM_CHESTPLATE;
+	public static RegistryObject<Item> DALEKANIUM_LEGGINGS;
+	public static RegistryObject<Item> DALEKANIUM_BOOTS;
+
+	public static RegistryObject<Item> REFINED_DALEKANIUM_HELMET;
+	public static RegistryObject<Item> REFINED_DALEKANIUM_CHESTPLATE;
+	public static RegistryObject<Item> REFINED_DALEKANIUM_LEGGINGS;
+	public static RegistryObject<Item> REFINED_DALEKANIUM_BOOTS;
+
+	public static RegistryObject<Item> PURE_DALEKANIUM_HELMET;
+	public static RegistryObject<Item> PURE_DALEKANIUM_CHESTPLATE;
+	public static RegistryObject<Item> PURE_DALEKANIUM_LEGGINGS;
+	public static RegistryObject<Item> PURE_DALEKANIUM_BOOTS;
+
+	public static RegistryObject<Item> METALERT_HELMET;
+	public static RegistryObject<Item> METALERT_CHESTPLATE;
+	public static RegistryObject<Item> METALERT_LEGGINGS;
+	public static RegistryObject<Item> METALERT_BOOTS;
 
 	static {
 		PISTOL = RegistryHandler.ITEMS.register("pistol",
@@ -32,5 +59,65 @@ public class DMAItems {
 
 		DINO_NUGGETS_CUSTARD = RegistryHandler.ITEMS.register("dino_nuggets_custard",
 			() -> new FoodItem((new Item.Properties()).food(DMAFoods.DINO_NUGGETS_CUSTARD).tab(ItemGroup.TAB_FOOD)));
+
+		STEEL_HELMET = RegistryHandler.ITEMS.register("steel_helmet",
+			() -> new ArmorItem(DMAArmorMaterial.STEEL, EquipmentSlotType.HEAD, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
+
+		STEEL_CHESTPLATE = RegistryHandler.ITEMS.register("steel_chestplate",
+			() -> new ArmorItem(DMAArmorMaterial.STEEL, EquipmentSlotType.CHEST, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
+
+		STEEL_LEGGINGS = RegistryHandler.ITEMS.register("steel_leggings",
+			() -> new ArmorItem(DMAArmorMaterial.STEEL, EquipmentSlotType.LEGS, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
+
+		STEEL_BOOTS = RegistryHandler.ITEMS.register("steel_boots",
+			() -> new ArmorItem(DMAArmorMaterial.STEEL, EquipmentSlotType.FEET, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
+
+		DALEKANIUM_HELMET = RegistryHandler.ITEMS.register("dalekanium_helmet",
+			() -> new ArmorItem(DMAArmorMaterial.DALEKANIUM, EquipmentSlotType.HEAD, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
+
+		DALEKANIUM_CHESTPLATE = RegistryHandler.ITEMS.register("dalekanium_chestplate",
+			() -> new ArmorItem(DMAArmorMaterial.DALEKANIUM, EquipmentSlotType.CHEST, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
+
+		DALEKANIUM_LEGGINGS = RegistryHandler.ITEMS.register("dalekanium_leggings",
+			() -> new ArmorItem(DMAArmorMaterial.DALEKANIUM, EquipmentSlotType.LEGS, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
+
+		DALEKANIUM_BOOTS = RegistryHandler.ITEMS.register("dalekanium_boots",
+			() -> new ArmorItem(DMAArmorMaterial.DALEKANIUM, EquipmentSlotType.FEET, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
+
+		REFINED_DALEKANIUM_HELMET = RegistryHandler.ITEMS.register("refined_dalekanium_helmet",
+			() -> new ArmorItem(DMAArmorMaterial.REFINED_DALEKANIUM, EquipmentSlotType.HEAD, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
+
+		REFINED_DALEKANIUM_CHESTPLATE = RegistryHandler.ITEMS.register("refined_dalekanium_chestplate",
+			() -> new ArmorItem(DMAArmorMaterial.REFINED_DALEKANIUM, EquipmentSlotType.CHEST, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
+
+		REFINED_DALEKANIUM_LEGGINGS = RegistryHandler.ITEMS.register("refined_dalekanium_leggings",
+			() -> new ArmorItem(DMAArmorMaterial.REFINED_DALEKANIUM, EquipmentSlotType.LEGS, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
+
+		REFINED_DALEKANIUM_BOOTS = RegistryHandler.ITEMS.register("refined_dalekanium_boots",
+			() -> new ArmorItem(DMAArmorMaterial.REFINED_DALEKANIUM, EquipmentSlotType.FEET, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
+
+		PURE_DALEKANIUM_HELMET = RegistryHandler.ITEMS.register("pure_dalekanium_helmet",
+			() -> new ArmorItem(DMAArmorMaterial.PURE_DALEKANIUM, EquipmentSlotType.HEAD, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
+
+		PURE_DALEKANIUM_CHESTPLATE = RegistryHandler.ITEMS.register("pure_dalekanium_chestplate",
+			() -> new ArmorItem(DMAArmorMaterial.PURE_DALEKANIUM, EquipmentSlotType.CHEST, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
+
+		PURE_DALEKANIUM_LEGGINGS = RegistryHandler.ITEMS.register("pure_dalekanium_leggings",
+			() -> new ArmorItem(DMAArmorMaterial.PURE_DALEKANIUM, EquipmentSlotType.LEGS, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
+
+		PURE_DALEKANIUM_BOOTS = RegistryHandler.ITEMS.register("pure_dalekanium_boots",
+			() -> new ArmorItem(DMAArmorMaterial.PURE_DALEKANIUM, EquipmentSlotType.FEET, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
+
+		METALERT_HELMET = RegistryHandler.ITEMS.register("metalert_helmet",
+			() -> new ArmorItem(DMAArmorMaterial.METALERT, EquipmentSlotType.HEAD, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
+
+		METALERT_CHESTPLATE = RegistryHandler.ITEMS.register("metalert_chestplate",
+			() -> new ArmorItem(DMAArmorMaterial.METALERT, EquipmentSlotType.CHEST, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
+
+		METALERT_LEGGINGS = RegistryHandler.ITEMS.register("metalert_leggings",
+			() -> new ArmorItem(DMAArmorMaterial.METALERT, EquipmentSlotType.LEGS, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
+
+		METALERT_BOOTS = RegistryHandler.ITEMS.register("metalert_boots",
+			() -> new ArmorItem(DMAArmorMaterial.METALERT, EquipmentSlotType.FEET, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
 	}
 }
