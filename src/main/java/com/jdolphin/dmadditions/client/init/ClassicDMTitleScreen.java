61329efeb185d374d9a3c14c129d95020db71295
James Dolphin <karl.kallip@gmail.com>
2022-10-30 15:15:37 +0200

    Configs(also testing older dm title screens(static))

---
+++ src/main/java/com/jdolphin/dmadditions/client/init/ClassicDMTitleScreen.java
@@ -0,0 +1,7 @@
+package com.jdolphin.dmadditions.client.init;
+
+import net.minecraft.client.gui.screen.MainMenuScreen;
+import com.swdteam.client.gui.title.RenderPanorama;
+
+public class ClassicDMTitleScreen {
+}
--- src/main/java/com/jdolphin/dmadditions/config/DMACommonConfig.java
+++ src/main/java/com/jdolphin/dmadditions/config/DMACommonConfig.java
@@ -1,4 +1,21 @@
 package com.jdolphin.dmadditions.config;
 
-public class DMACommonConfig {
+import net.minecraftforge.common.ForgeConfigSpec;
+
+public final class DMACommonConfig {
+	public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+	public static final ForgeConfigSpec SPEC;
+
+	public static final ForgeConfigSpec.ConfigValue<Integer> randomizer_max;
+	public static final ForgeConfigSpec.ConfigValue<Boolean> dma_title;
+
+	static {
+		BUILDER.push("Dalek Mod: Additions Config");
+
+		randomizer_max = BUILDER.comment("Maximum distance for randomizer. Deafult = 10000").define("randomizer_max", 10000);
+		dma_title = BUILDER.comment("Use Dalek Mod: Additions title screen. Deafult = true").define("dma_title_classic", true);
+
+		BUILDER.pop();
+		SPEC = BUILDER.build();
+	}
 }
