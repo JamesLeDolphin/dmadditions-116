2de09b9729c5fd07eaaf16446f013ff8b26b6852
James Dolphin <karl.kallip@gmail.com>
2022-10-30 15:43:19 +0200

    Renamed them to background_1-9, messed around in ClassicDMTitleScreen class

--- src/main/java/com/jdolphin/dmadditions/client/init/ClassicDMTitleScreen.java
+++ src/main/java/com/jdolphin/dmadditions/client/init/ClassicDMTitleScreen.java
@@ -1,9 +1,37 @@
 package com.jdolphin.dmadditions.client.init;
 
-import com.jdolphin.dmadditions.config.DMACommonConfig;
-import net.minecraft.client.gui.screen.MainMenuScreen;
-import com.swdteam.client.gui.title.RenderPanorama;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
-public class ClassicDMTitleScreen {
+@OnlyIn(Dist.CLIENT)
+public class ClassicDMTitleScreen extends Screen {
+	private final ResourceLocation[] images = new ResourceLocation[9];
+	private String splash;
 
+	protected ClassicDMTitleScreen(ITextComponent p_i51108_1_) {
+		super(p_i51108_1_);
+	}
+
+	public void RenderBackground() {
+		for(int i = 0; i < 9; ++i) {
+			this.images[i] = new ResourceLocation("dmadditions", "textures/gui/main/background" + i + ".png");
+		}
+
+	}
+	public boolean isPauseScreen() {
+		return false;
+	}
+
+	public boolean shouldCloseOnEsc() {
+		return false;
+	}
+	protected void init() {
+		if (this.splash == null) {
+			this.splash = this.minecraft.getSplashManager().getSplash();
+		}
+
+	}
 }
