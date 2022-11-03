ce4ec30fd7418a9ae3c16207ce1be3d50ebe69c7
SamTees <26529248+SamTees@users.noreply.github.com>
2022-11-03 15:44:55 +0000

    remove event listeners from main class

--- src/main/java/com/jdolphin/dmadditions/DmAdditions.java
+++ src/main/java/com/jdolphin/dmadditions/DmAdditions.java
@@ -1,28 +1,16 @@
 package com.jdolphin.dmadditions;
 
-import com.jdolphin.dmadditions.advent.AdventUnlock;
-import com.jdolphin.dmadditions.config.DMAClientConfig;
-import com.jdolphin.dmadditions.config.DMACommonConfig;
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
-import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.stream.Collectors;
+import com.jdolphin.dmadditions.advent.AdventUnlock;
+import com.jdolphin.dmadditions.config.DMAClientConfig;
+import com.jdolphin.dmadditions.config.DMACommonConfig;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("dmadditions")
@@ -35,16 +23,6 @@ public class DmAdditions {
 
 
 	public DmAdditions() {
-		// Register the setup method for modloading
-		IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
-		eventBus.addListener(this::setup);
-		// Register the enqueueIMC method for modloading
-		eventBus.addListener(this::enqueueIMC);
-		// Register the processIMC method for modloading
-		eventBus.addListener(this::processIMC);
-		// Register the doClientStuff method for modloading
-		eventBus.addListener(this::doClientStuff);
-
 		// Register things
 		RegistryHandler.init();
 		ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, DMAClientConfig.SPEC, "dma-client.toml");
@@ -52,49 +30,5 @@ public class DmAdditions {
 		// Register ourselves for server and other game events we are interested in
 		MinecraftForge.EVENT_BUS.register(this);
 
-	}
-
-	private void setup(final FMLCommonSetupEvent event) {
-		// some preinit code
-		LOGGER.info("HELLO FROM PREINIT");
-		LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
-	}
-
-	private void doClientStuff(final FMLClientSetupEvent event) {
-		// do something that can only be done on the client
-		LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().options);
-	}
-
-	private void enqueueIMC(final InterModEnqueueEvent event) {
-		// some example code to dispatch IMC to another mod
-		InterModComms.sendTo("examplemod", "helloworld", () -> {
-			LOGGER.info("Hello world from the MDK");
-			return "Hello world";
-		});
-	}
-
-	private void processIMC(final InterModProcessEvent event) {
-		// some example code to receive and process InterModComms from other mods
-		LOGGER.info("Got IMC {}", event.getIMCStream().
-				map(m -> m.getMessageSupplier().get()).
-				collect(Collectors.toList()));
-	}
-
-	// You can use SubscribeEvent and let the Event Bus discover methods to call
-	@SubscribeEvent
-	public void onServerStarting(FMLServerStartingEvent event) {
-		// do something when the server starts
-		LOGGER.info("HELLO from server starting");
-	}
-
-	// You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-	// Event bus for receiving Registry Events)
-	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
-	public static class RegistryEvents {
-		@SubscribeEvent
-		public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-			// register a new block here
-			LOGGER.info("HELLO from Register Block");
-		}
 	}
 }
