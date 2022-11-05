012bf35eedf1a7cf0f21dee3bc074fddd110e8d0
James Dolphin <karl.kallip@gmail.com>
2022-11-05 16:38:59 +0200

    worked on australian interior

---
+++ src/main/java/com/jdolphin/dmadditions/entity/dalek/types/CandycaneDalek.java
@@ -0,0 +1,28 @@
+package com.jdolphin.dmadditions.entity.dalek.types;
+
+import com.swdteam.common.entity.dalek.DalekBase;
+import com.swdteam.common.entity.dalek.DalekEntity;
+import com.swdteam.common.init.DMProjectiles;
+import com.swdteam.common.init.DMSoundEvents;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.SoundEvent;
+
+public class CandycaneDalek extends DalekBase {
+	public CandycaneDalek(String dalekName) {
+		super(dalekName);
+	}
+		public float getMaxHealth() {
+			return 15.0F;
+		}
+
+		public DMProjectiles.Laser getLaser(DalekEntity dalek) {
+			return DMProjectiles.GREEN_LASER;
+		}
+
+		public SoundEvent getAttackSound(Entity e) {
+			return (SoundEvent) DMSoundEvents.ENTITY_DALEK_CLASSIC_ATTACK.get();
+		}
+
+		protected void aiStep() {
+		}
+}
