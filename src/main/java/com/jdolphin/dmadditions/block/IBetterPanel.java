03d7af56ee2eda9a6889980c1063fe7dcc7e0972
James Dolphin <karl.kallip@gmail.com>
2022-11-16 16:06:43 +0200

    Removed ItemPropertiesMixin(mixin.json) and fixed TardisKeyMixin

--- src/main/java/com/jdolphin/dmadditions/block/IBetterPanel.java
+++ src/main/java/com/jdolphin/dmadditions/block/IBetterPanel.java
@@ -1,24 +1,18 @@
 package com.jdolphin.dmadditions.block;
 
 import com.swdteam.common.block.AbstractRotateableWaterLoggableBlock;
-import com.swdteam.common.block.RotatableTileEntityBase;
-import net.minecraft.block.*;
+import com.swdteam.common.block.IBlockTooltip;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.fluid.FluidState;
-import net.minecraft.fluid.Fluids;
 import net.minecraft.item.BlockItemUseContext;
-import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.DirectionProperty;
-import net.minecraft.state.Property;
-import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.AttachFace;
-import net.minecraft.state.properties.BlockStateProperties;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
-import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.world.IBlockReader;
@@ -27,13 +21,8 @@ import net.minecraft.world.IWorldReader;
 
 import javax.annotation.Nullable;
 
-import java.util.function.Supplier;
-
-import static com.swdteam.common.block.AbstractRotateableWaterLoggableBlock.WATERLOGGED;
-
 public interface IBetterPanel extends IHorizontalFaceBlock, IBetterBlockTooltip {
 	DirectionProperty FACING = AbstractRotateableWaterLoggableBlock.FACING;
-
 
 	VoxelShape EAST_AABB = Block.box(0.0D, 0.0D, 0.0D, 3.0D, 16.0D, 16.0D);
 	VoxelShape WEST_AABB = Block.box(13.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D);
@@ -85,70 +74,25 @@ public interface IBetterPanel extends IHorizontalFaceBlock, IBetterBlockTooltip 
 	@Nullable
 	@Override
 	default BlockState getStateForPlacement(BlockItemUseContext context) {
-		Direction direction = context.getClickedFace();
-		BlockPos blockpos = context.getClickedPos();
-		BlockState blockstate;
-		if (direction.getAxis() == Direction.Axis.Y) {
-			blockstate = this.defaultBlockState().setValue(FACE, direction == Direction.UP ? AttachFace.CEILING : AttachFace.FLOOR).setValue(FACING, context.getHorizontalDirection());
-		} else {
-			blockstate = this.defaultBlockState().setValue(FACE, AttachFace.WALL).setValue(FACING, direction.getOpposite());
+		for (Direction direction : context.getNearestLookingDirections()) {
+			BlockState blockstate;
+			if (direction.getAxis() == Direction.Axis.Y) {
+				blockstate = this.defaultBlockState().setValue(FACE, direction == Direction.UP ? AttachFace.CEILING : AttachFace.FLOOR).setValue(FACING, context.getHorizontalDirection().getOpposite());
+			} else {
+				blockstate = this.defaultBlockState().setValue(FACE, AttachFace.WALL).setValue(FACING, direction.getOpposite());
+			}
+
+			if (blockstate.canSurvive(context.getLevel(), context.getClickedPos())) {
+				return blockstate;
+			}
 		}
 
-		if (blockstate.canSurvive(context.getLevel(), context.getClickedPos())) {
-			return blockstate;
-
-		}
 		return null;
 	}
-
-
 
 	@Override
 	default ITextComponent getName(BlockState blockState, BlockPos blockPos, Vector3d vector3d, PlayerEntity playerEntity) {
 		return IBetterBlockTooltip.super.getName(blockState, blockPos, vector3d, playerEntity);
-	}
-	public static class WaterLoggable extends RotatableTileEntityBase implements IWaterLoggable {
-		public static final BooleanProperty WATERLOGGED;
 
-		public WaterLoggable(Supplier<TileEntity> tileEntitySupplier, int light) {
-			super(tileEntitySupplier, light);
-			this.registerDefaultState((BlockState)super.defaultBlockState().setValue(WATERLOGGED, false));
-		}
-
-		public WaterLoggable(Supplier<TileEntity> tileEntitySupplier, AbstractBlock.Properties properties) {
-			super(tileEntitySupplier, properties);
-			this.registerDefaultState((BlockState)super.defaultBlockState().setValue(WATERLOGGED, false));
-		}
-
-		public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
-			return VoxelShapes.box(0.0, 0.0, 0.0, 0.9999, 1.0, 0.9999);
-		}
-
-		public BlockState getStateForPlacement(BlockItemUseContext context) {
-			BlockPos blockpos = context.getClickedPos();
-			FluidState fluidstate = context.getLevel().getFluidState(blockpos);
-			return (BlockState)super.getStateForPlacement(context).setValue(WATERLOGGED, fluidstate.getType() == Fluids.WATER);
-		}
-
-		public BlockState updateShape(BlockState p_196271_1_, Direction p_196271_2_, BlockState p_196271_3_, IWorld p_196271_4_, BlockPos p_196271_5_, BlockPos p_196271_6_) {
-			if ((Boolean)p_196271_1_.getValue(WATERLOGGED)) {
-				p_196271_4_.getLiquidTicks().scheduleTick(p_196271_5_, Fluids.WATER, Fluids.WATER.getTickDelay(p_196271_4_));
-			}
-
-			return super.updateShape(p_196271_1_, p_196271_2_, p_196271_3_, p_196271_4_, p_196271_5_, p_196271_6_);
-		}
-
-		public FluidState getFluidState(BlockState state) {
-			return (Boolean)state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
-		}
-
-		protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> state) {
-			super.createBlockStateDefinition(state);
-			state.add(new Property[]{WATERLOGGED});
-		}
-
-		static {
-			WATERLOGGED = BlockStateProperties.WATERLOGGED;
-		}
 	}
 }
--- src/main/java/com/jdolphin/dmadditions/mixin/TardisKeyMixin.java
+++ src/main/java/com/jdolphin/dmadditions/mixin/TardisKeyMixin.java
@@ -1,24 +1,19 @@
 package com.jdolphin.dmadditions.mixin;
 
-import com.swdteam.common.init.DMTabs;
 import com.swdteam.common.item.TardisKeyItem;
 import net.minecraft.item.Item;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TardisKeyItem.class)
 abstract class TardisKeyMixin {
-	/*@Shadow
-	String tardisLocation;
+
 
 	@Redirect(method = "<init>(Lnet/minecraft/item/Item$Properties;Ljava/lang/String;)V",
-		at = @At(value = "INVOKE_STRING",
-			args = "ldc=tick"))
-	private void TardisKeyItem(Item.Properties properties, String tardisLocation) {
-		return properties.tab(DMTabs.DM_TARDIS);
-		this.tardisLocation = tardisLocation;
-	}*/
+		at = @At(value = "INVOKE", target = "Lnet/minecraft/item/Item$Properties;stacksTo(I)Lnet/minecraft/item/Item$Properties;"))
+
+	private static Item.Properties TardisKeyItem(Item.Properties properties, int x) {
+		return properties;
+	}
 }
