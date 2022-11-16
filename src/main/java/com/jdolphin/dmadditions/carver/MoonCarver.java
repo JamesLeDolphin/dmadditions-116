e1512e6e1201255e20d8869cfd341d0b93872c56
James Dolphin <karl.kallip@gmail.com>
2022-11-16 18:24:44 +0200

    MoonCarver

--- src/main/java/com/jdolphin/dmadditions/carver/MoonCarver.java
+++ src/main/java/com/jdolphin/dmadditions/carver/MoonCarver.java
@@ -1,11 +1,14 @@
 package com.jdolphin.dmadditions.carver;
 
+import com.google.common.collect.ImmutableSet;
 import com.mojang.serialization.Codec;
-import com.swdteam.common.carver.ClassicWorldCarver;
+import com.swdteam.common.init.DMBlocks;
+import net.minecraft.world.gen.carver.CaveWorldCarver;
 import net.minecraft.world.gen.feature.ProbabilityConfig;
 
-public class MoonCarver extends ClassicWorldCarver {
+public class MoonCarver extends CaveWorldCarver {
 	public MoonCarver(Codec<ProbabilityConfig> codec) {
-		super(codec);
+		super(codec, 256);
+		this.replaceableBlocks = ImmutableSet.of(DMBlocks.ANORTHOSITE.get(), DMBlocks.METEORITE.get());
 	}
 }
