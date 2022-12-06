3239a5e0ed4f5232073fbd26d67f404218e7dc23
James Dolphin <karl.kallip@gmail.com>
2022-12-06 16:05:47 +0200

    Gravity and unit gun recipe test

--- src/main/java/com/jdolphin/dmadditions/dimension/Gravity.java
+++ src/main/java/com/jdolphin/dmadditions/dimension/Gravity.java
@@ -6,22 +6,27 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.player.ClientPlayerEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.ai.attributes.Attributes;
-import net.minecraft.entity.ai.attributes.ModifiableAttributeInstance;
+import net.minecraft.entity.ai.attributes.*;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 
+import javax.annotation.Nullable;
+
+import java.util.UUID;
+
 import static net.minecraft.potion.Effects.SLOW_FALLING;
 import static net.minecraftforge.common.ForgeMod.ENTITY_GRAVITY;
 
-public class Gravity {
-
-	public static void moonGravity(LivingEntity living) {
+public class Gravity  {
+	private static final UUID SLOW_FALLING_ID = UUID.fromString("A5B6CF2A-2F7C-31EF-9022-7C3E7D5E6ABA");
+	private static final AttributeModifier SLOW_FALLING = new AttributeModifier(SLOW_FALLING_ID, "Slow falling acceleration reduction", -0.07, AttributeModifier.Operation.ADDITION);
+	public void moonGravity(LivingEntity living) {
 		if (living.getEntity().level.dimension() == DMADimensions.MOON) {
-			living.getAttribute(Attributes.JUMP_STRENGTH);
-			living.getEffect(Effects.SLOW_FALLING).applyEffect(living); //new EffectInstance(Effects.SLOW_FALLING, 500, 70).applyEffect(living);
-			new EffectInstance(Effects.JUMP, 500, 70).applyEffect(living);
+			ModifiableAttributeInstance gravity = living.getAttribute(net.minecraftforge.common.ForgeMod.ENTITY_GRAVITY.get());
+			 gravity.addTransientModifier(SLOW_FALLING);
+
+
 		}
 	}
 }
