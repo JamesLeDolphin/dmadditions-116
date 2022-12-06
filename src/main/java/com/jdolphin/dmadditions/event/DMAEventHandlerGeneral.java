b07692ff3179240183dfb16e0c6d968eb198d358
SamTees <26529248+SamTees@users.noreply.github.com>
2022-12-06 18:56:44 +0000

    Set gravity for new entities

--- src/main/java/com/jdolphin/dmadditions/event/DMAEventHandlerGeneral.java
+++ src/main/java/com/jdolphin/dmadditions/event/DMAEventHandlerGeneral.java
@@ -3,6 +3,9 @@ package com.jdolphin.dmadditions.event;
 import com.jdolphin.dmadditions.dimension.Gravity;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.RegistryKey;
+import net.minecraft.world.World;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.EntityTravelToDimensionEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -16,5 +19,19 @@ public class DMAEventHandlerGeneral {
 		if (entity.level.isClientSide) return;
 
 		Gravity.changeGravity((LivingEntity) entity, event.getDimension(), entity.level.dimension());
+	}
+
+	@SubscribeEvent
+	public static void onEntityJoinWorld(EntityJoinWorldEvent event) {
+		if (event.getWorld().isClientSide) return;
+
+		Entity entity = event.getEntity();
+		if (!(entity instanceof LivingEntity)) return;
+		if (entity.tickCount > 0) return;
+
+		RegistryKey<World> dimension = event.getWorld().dimension();
+		if (!Gravity.DIMENSION_GRAVITY.containsKey(dimension)) return;
+
+		Gravity.changeGravity((LivingEntity) entity, dimension, null);
 	}
 }
