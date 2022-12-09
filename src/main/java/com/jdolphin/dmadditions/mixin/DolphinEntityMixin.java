1ab22a9424936a9397c2560c24071bb128ea180a
SamTees <26529248+SamTees@users.noreply.github.com>
2022-12-09 23:51:39 +0000

    Make dolphins turn into james when named james/jimothy

---
+++ src/main/java/com/jdolphin/dmadditions/mixin/DolphinEntityMixin.java
@@ -0,0 +1,54 @@
+package com.jdolphin.dmadditions.mixin;
+
+import com.jdolphin.dmadditions.entity.JamesLeDolphinEntity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.passive.DolphinEntity;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+
+import javax.annotation.Nullable;
+
+import static com.jdolphin.dmadditions.init.DMAEntities.JAMESLEDOLPHIN;
+
+@Mixin(DolphinEntity.class)
+public abstract class DolphinEntityMixin extends LivingEntity {
+
+	protected DolphinEntityMixin(EntityType<? extends LivingEntity> p_i48577_1_, World p_i48577_2_) {
+		super(p_i48577_1_, p_i48577_2_);
+	}
+
+	protected void checkName(ITextComponent name){
+		if (name != null && name.getString()
+			.matches("(?i)1?(james|jimothy)(le)?(dolphin)?")) {
+
+			DolphinEntity dolphin = (DolphinEntity) (Object) this;
+
+			JamesLeDolphinEntity james = new JamesLeDolphinEntity(JAMESLEDOLPHIN.get(), dolphin.level);
+			james.setCustomName(this.getCustomName());
+			james.setHealth(this.getHealth());
+			james.setBaby(this.isBaby());
+			james.setDeltaMovement(this.getDeltaMovement());
+			james.setGotFish(dolphin.gotFish());
+			james.setTreasurePos(dolphin.getTreasurePos());
+			james.setInvulnerable(this.isInvulnerable());
+			james.copyPosition(this);
+
+			this.remove();
+			this.level.addFreshEntity(james);
+		}
+
+	}
+
+	@Override
+	public void setCustomName(@Nullable ITextComponent component) {
+		super.setCustomName(component);
+
+		if (this.level.isClientSide || this.getType().equals(JAMESLEDOLPHIN.get()) || !this.isAddedToWorld())
+			return;
+
+		checkName(component);
+	}
+
+}
