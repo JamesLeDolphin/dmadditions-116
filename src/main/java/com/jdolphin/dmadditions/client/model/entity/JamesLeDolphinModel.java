b75112d245c044457cdc6b82ca190c5bc218344b
SamTees <26529248+SamTees@users.noreply.github.com>
2022-12-10 00:13:37 +0000

    Add animations to jamesledolphin

--- src/main/java/com/jdolphin/dmadditions/client/model/entity/JamesLeDolphinModel.java
+++ src/main/java/com/jdolphin/dmadditions/client/model/entity/JamesLeDolphinModel.java
@@ -9,8 +9,10 @@ import com.swdteam.model.javajson.ModelWrapper;
 import net.minecraft.client.renderer.entity.model.DolphinModel;
 import net.minecraft.client.renderer.entity.model.IHasHead;
 import net.minecraft.client.renderer.model.ModelRenderer;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.passive.DolphinEntity;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.MathHelper;
 
 import javax.annotation.Nonnull;
 
@@ -52,5 +54,17 @@ public class JamesLeDolphinModel<T extends DolphinEntity> extends DolphinModel<T
 	@Override
 	public ModelRenderer getHead() {
 		return this.head;
+	}
+
+
+	@Override
+	public void setupAnim(T p_225597_1_, float p_225597_2_, float p_225597_3_, float p_225597_4_, float p_225597_5_, float p_225597_6_) {
+		this.body.xRot = p_225597_6_ * ((float) Math.PI / 180F);
+		this.body.yRot = p_225597_5_ * ((float) Math.PI / 180F);
+		if (Entity.getHorizontalDistanceSqr(p_225597_1_.getDeltaMovement()) > 1.0E-7D) {
+			this.body.xRot += -0.05F + -0.05F * MathHelper.cos(p_225597_4_ * 0.3F);
+			this.tail.xRot = -0.1F * MathHelper.cos(p_225597_4_ * 0.3F);
+			this.tailFin.xRot = -0.2F * MathHelper.cos(p_225597_4_ * 0.3F);
+		}
 	}
 }
