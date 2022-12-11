278b6c30f65c540e92fed022c915f0244ad9bb79
James Dolphin <karl.kallip@gmail.com>
2022-12-11 18:53:04 +0200

    Broken structures

--- src/main/java/com/jdolphin/dmadditions/DmAdditions.java
+++ src/main/java/com/jdolphin/dmadditions/DmAdditions.java
@@ -10,14 +10,20 @@ import com.jdolphin.dmadditions.entity.PilotFishEntity;
 import com.jdolphin.dmadditions.entity.SnowmanEntity;
 import com.jdolphin.dmadditions.entity.WoodenCybermanEntity;
 import com.jdolphin.dmadditions.event.DMAEventHandlerGeneral;
-import com.jdolphin.dmadditions.init.DMABlocks;
-import com.jdolphin.dmadditions.init.DMAEntities;
-import com.jdolphin.dmadditions.init.DMAItems;
-import com.jdolphin.dmadditions.init.DMASpawnerRegistry;
+import com.jdolphin.dmadditions.init.*;
+import com.jdolphin.dmadditions.structure.DMAConfiguredStructures;
+import com.swdteam.common.init.DMStructures;
+import com.swdteam.common.structure.DMConfiguredStructures;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraft.entity.ai.attributes.GlobalEntityTypeAttributes;
+import net.minecraft.util.RegistryKey;
+import net.minecraft.util.registry.WorldGenRegistries;
 import net.minecraft.world.biome.MobSpawnInfo;
+import net.minecraft.world.gen.DimensionSettings;
+import net.minecraft.world.gen.feature.structure.Structure;
+import net.minecraft.world.gen.settings.DimensionStructuresSettings;
+import net.minecraft.world.gen.settings.StructureSeparationSettings;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
@@ -28,11 +34,14 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.List;
+
+import static com.swdteam.main.DalekMod.registerStructure;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("dmadditions")
@@ -54,7 +63,9 @@ public class DmAdditions {
 		LOGGER.info(IS_DEBUG ? "Running in debugger" : "Not running in debugger");
 		modEventBus.addListener(this::setup);
 		DMAItems.ITEMS.register(modEventBus);
+		DMStructures.DEFERRED_REGISTRY_STRUCTURE.register(modEventBus);
 		modEventBus.addListener(this::doClientStuff);
+		modEventBus.addListener(this::addStructures);
 		// Register things
 		RegistryHandler.init();
 		ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, DMAClientConfig.SPEC, "dma-client.toml");
@@ -70,7 +81,10 @@ public class DmAdditions {
 	private void setup(FMLCommonSetupEvent event) {
 		DMASpawnerRegistry.init();
 		IRustToo.addRustedVariants();
+		
 		event.enqueueWork(() -> {
+			DMAStructures.setupStructures();
+			DMAConfiguredStructures.registerConfiguredStructures();
 			GlobalEntityTypeAttributes.put(DMAEntities.JAMESLEDOLPHIN.get(), JamesLeDolphinEntity.createAttributes().build());
 
 			if (DMAEntities.WOODEN_CYBERMAN != null)
@@ -102,5 +116,22 @@ public class DmAdditions {
 				spawns.add(spawn.spawner);
 			}
 		}
+		event.getGeneration().getStructures().add(() -> {
+			return DMAConfiguredStructures.CONFIGURED_SPACESHIP_1;
+		});
+		event.getGeneration().getStructures().add(() -> {
+			return DMAConfiguredStructures.CONFIGURED_SPACESHIP_2;
+		});
+		event.getGeneration().getStructures().add(() -> {
+			return DMAConfiguredStructures.CONFIGURED_SPACESHIP_3;
+		});
 	}
+	public void addStructures(FMLLoadCompleteEvent event) {
+		event.enqueueWork(() -> {
+			registerStructure(DimensionSettings.OVERWORLD, DMAStructures.SPACESHIP_1.get(), DimensionStructuresSettings.DEFAULTS.get(DMAStructures.SPACESHIP_1.get()));
+			registerStructure(DimensionSettings.OVERWORLD, DMAStructures.SPACESHIP_2.get(), DimensionStructuresSettings.DEFAULTS.get(DMAStructures.SPACESHIP_2.get()));
+			registerStructure(DimensionSettings.OVERWORLD, DMAStructures.SPACESHIP_3.get(), DimensionStructuresSettings.DEFAULTS.get(DMAStructures.SPACESHIP_3.get()));
+		});
+	}
+
 }
