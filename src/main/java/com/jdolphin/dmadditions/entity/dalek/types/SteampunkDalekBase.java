e3f6719caa16828b26ccd57f58f48d864c27517d
James Dolphin <karl.kallip@gmail.com>
2022-12-19 19:25:06 +0200

    Steampunk daleks

---
+++ src/main/java/com/jdolphin/dmadditions/entity/dalek/types/SteampunkDalekBase.java
@@ -0,0 +1,33 @@
+package com.jdolphin.dmadditions.entity.dalek.types;
+
+import com.swdteam.common.entity.dalek.DalekBase;
+import com.swdteam.common.entity.dalek.DalekEntity;
+import com.swdteam.common.init.DMParticleTypes;
+import com.swdteam.common.init.DMProjectiles;
+import com.swdteam.common.init.DMSoundEvents;
+import net.minecraft.entity.Entity;
+import net.minecraft.particles.IParticleData;
+import net.minecraft.util.SoundEvent;
+
+public class SteampunkDalekBase extends DalekBase {
+	public SteampunkDalekBase(String dalekName) {
+		super(dalekName);
+	}
+	public float getMaxHealth() {
+		return 15.0F;
+	}
+
+	public DMProjectiles.Laser getLaser(DalekEntity dalek) {
+		return DMProjectiles.SMOKE;
+	}
+	public SoundEvent getAttackSound(Entity e) {
+	return DMSoundEvents.ENTITY_DALEK_MOLTEN_ATTACK.get();
+	}
+	public SoundEvent getShootSound(Entity e) {
+		return DMSoundEvents.ENTITY_DALEK_GUNSTICK_SHOOT.get();
+	}
+
+
+	protected void aiStep() {
+	}
+}
