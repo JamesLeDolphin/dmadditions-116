94b5eb53f362280c7e82487b75fc47583db40ab6
JamesLeDolphin <karl.kallip@gmail.com>
2023-02-28 14:10:22 +0200

    Cant join dmu when dma is installed

--- src/main/java/com/jdolphin/dmadditions/event/JoinServerEvent.java
+++ src/main/java/com/jdolphin/dmadditions/event/JoinServerEvent.java
@@ -8,6 +8,8 @@ import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.entity.player.ServerPlayerEntity;
 
+import java.util.Objects;
+
 
 @Mod.EventBusSubscriber
 public class JoinServerEvent {
@@ -15,7 +17,12 @@ public class JoinServerEvent {
 	@SubscribeEvent
 	public static void onPlayerLoggedIn(PlayerLoggedInEvent event) {
 		ServerPlayerEntity player = (ServerPlayerEntity) event.getPlayer();
+		if (Objects.requireNonNull(player.getServer()).getLocalIp().equals("dmu.swdteam.co.uk:25565")) {
+			player.connection.disconnect(new StringTextComponent("Please remove DMA before joining DMU"));
+		} else {
 			ITextComponent message = new StringTextComponent("Using DMA on this server may have adverse side effects.").withStyle(TextFormatting.BOLD).withStyle(TextFormatting.RED);
 			player.sendMessage(message, player.getUUID());
-	}
+		}
+
+		}
 }
