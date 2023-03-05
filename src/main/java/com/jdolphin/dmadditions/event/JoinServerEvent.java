050951d34234602b8eedb05aa06d985eadff2ef3
JamesLeDolphin <karl.kallip@gmail.com>
2023-03-05 20:30:20 +0200

    custom tardis door stuff. ye happy Acen?

--- src/main/java/com/jdolphin/dmadditions/event/JoinServerEvent.java
+++ src/main/java/com/jdolphin/dmadditions/event/JoinServerEvent.java
@@ -1,6 +1,7 @@
 package com.jdolphin.dmadditions.event;
 
 import net.minecraft.util.text.TextFormatting;
+import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.event.entity.player.PlayerEvent.PlayerLoggedInEvent;
@@ -17,12 +18,16 @@ public class JoinServerEvent {
 	@SubscribeEvent
 	public static void onPlayerLoggedIn(PlayerLoggedInEvent event) {
 		ServerPlayerEntity player = (ServerPlayerEntity) event.getPlayer();
-		if (Objects.requireNonNull(player.getServer()).getLocalIp().equals("dmu.swdteam.co.uk:25565")) {
+		if (player.connection.connection.getRemoteAddress().equals("dmu.swdteam.co.uk:25565")) {
 			player.connection.disconnect(new StringTextComponent("Please remove DMA before joining DMU"));
+			player.connection.connection.handleDisconnection();
+			event.setCanceled(true);
+			event.setResult(Event.Result.DENY);
+
 		} else {
-			ITextComponent message = new StringTextComponent("Using DMA on this server may have adverse side effects.").withStyle(TextFormatting.BOLD).withStyle(TextFormatting.RED);
+			ITextComponent message = new StringTextComponent("Using DMA on this server may have adverse side effects.");
 			player.sendMessage(message, player.getUUID());
 		}
+	}
 
-		}
 }
