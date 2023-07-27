51de6595bdf735d538c61f94616fb0d619057d82
JamesLeDolphin <karl.kallip@gmail.com>
2023-07-27 10:10:42 +0300

    add config to disable swd explosive laser

--- src/main/java/com/jdolphin/dmadditions/config/DMACommonConfig.java
+++ src/main/java/com/jdolphin/dmadditions/config/DMACommonConfig.java
@@ -8,12 +8,14 @@ public final class DMACommonConfig {
 
 	public static final ForgeConfigSpec.ConfigValue<Integer> randomizer_max;
 	public static final ForgeConfigSpec.ConfigValue<Boolean> disable_spawns;
+	public static final ForgeConfigSpec.ConfigValue<Boolean> disable_swd_laser;
 
 	static {
 		BUILDER.push("Dalek Mod: Additions Common Config");
 
-		randomizer_max = BUILDER.comment("Maximum distance for randomizer. Deafult = 10000").define("randomizer_max", 10000);
-		disable_spawns = BUILDER.comment("Disable the spawning of Dalek Mod mobs. Deafult = false").define("disable_spawns", false);
+		randomizer_max = BUILDER.comment("Maximum distance for randomizer. Default = 10000").define("randomizer_max", 10000);
+		disable_spawns = BUILDER.comment("Disable the spawning of Dalek Mod mobs. Default = false").define("disable_spawns", false);
+		disable_swd_laser = BUILDER.comment("Disable special weapons daleks explosive laser. Default = false").define("disable_swd_laser", true);
 
 		BUILDER.pop();
 		SPEC = BUILDER.build();
---
+++ src/main/java/com/jdolphin/dmadditions/mixin/SpecialWeaponsMixin.java
@@ -0,0 +1,24 @@
+package com.jdolphin.dmadditions.mixin;
+
+import com.jdolphin.dmadditions.config.DMACommonConfig;
+import com.swdteam.common.entity.dalek.DalekEntity;
+import com.swdteam.common.entity.dalek.types.SpecialWeapons;
+import com.swdteam.common.init.DMProjectiles;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+
+@Mixin(SpecialWeapons.class)
+public class SpecialWeaponsMixin {
+
+
+	@Inject(method = "getLaser(Lcom/swdteam/common/entity/dalek/DalekEntity;)Lcom/swdteam/common/init/DMProjectiles$Laser;", at = @At("HEAD"), remap = false, cancellable = true)
+	public void getLaser(DalekEntity dalek, CallbackInfoReturnable<DMProjectiles.Laser> cir) {
+		if (DMACommonConfig.disable_swd_laser.get()) {
+			cir.setReturnValue(DMProjectiles.BLUE_LASER);
+		}
+		else cir.setReturnValue(DMProjectiles.EXPLOSIVE_LASER);
+	}
+}
