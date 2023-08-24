f29d0624f2fd02615aeb8b0fe2f75539ec7259a0
Torchwood-1 <jacobroberts@virginmedia.com>
2023-08-24 13:06:43 +0100

    i hate my life

---
+++ src/main/java/com/jdolphin/dmadditions/carver/AndrozaniminorCarver.java
@@ -0,0 +1,14 @@
+package com.jdolphin.dmadditions.carver;
+
+import com.google.common.collect.ImmutableSet;
+import com.mojang.serialization.Codec;
+import net.minecraft.block.Blocks;
+import net.minecraft.world.gen.carver.CaveWorldCarver;
+import net.minecraft.world.gen.feature.ProbabilityConfig;
+
+public class AndrozaniminorCarver extends CaveWorldCarver {
+	public AndrozaniminorCarver(Codec<ProbabilityConfig> codec) {
+		super(codec, 256);
+		this.replaceableBlocks = ImmutableSet.of(Blocks.SNOW_BLOCK, Blocks.GRASS_BLOCK);
+	}
+}
--- src/main/java/com/jdolphin/dmadditions/init/DMAWorldCarvers.java
+++ src/main/java/com/jdolphin/dmadditions/init/DMAWorldCarvers.java
@@ -1,5 +1,6 @@
 package com.jdolphin.dmadditions.init;
 
+import com.jdolphin.dmadditions.carver.AndrozaniminorCarver;
 import com.jdolphin.dmadditions.carver.MoonCarver;
 import com.swdteam.common.RegistryHandler;
 import net.minecraft.world.gen.feature.ProbabilityConfig;
@@ -7,6 +8,7 @@ import net.minecraftforge.fml.RegistryObject;
 
 public class DMAWorldCarvers {
 	public static final RegistryObject<MoonCarver> CARVER;
+	public static final RegistryObject<AndrozaniminorCarver> ANDROZANIMINOR_CARVER;
 
 	public DMAWorldCarvers() {
 	}
@@ -15,5 +17,9 @@ public class DMAWorldCarvers {
 		CARVER = RegistryHandler.WORLD_CARVERS.register("moon_carver", () -> {
 			return new MoonCarver(ProbabilityConfig.CODEC);
 		});
+		ANDROZANIMINOR_CARVER = RegistryHandler.WORLD_CARVERS.register("androzaniminor_carver", () -> {
+			return new AndrozaniminorCarver(ProbabilityConfig.CODEC);
+		});
+
 	}
 }
