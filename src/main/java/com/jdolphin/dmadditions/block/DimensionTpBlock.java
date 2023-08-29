31fd9c1cc28f89b9f1ef589953d257fea8714e25
JamesLeDolphin <karl.kallip@gmail.com>
2023-08-29 10:18:57 +0300

    edit yer thingy

--- src/main/java/com/jdolphin/dmadditions/block/DimensionTpBlock.java
+++ src/main/java/com/jdolphin/dmadditions/block/DimensionTpBlock.java
@@ -1,10 +1,8 @@
 package com.jdolphin.dmadditions.block;
 
+import com.jdolphin.dmadditions.init.DMABlocks;
 import com.jdolphin.dmadditions.init.DMADimensions;
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.NetherPortalBlock;
+import net.minecraft.block.*;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.server.MinecraftServer;
@@ -31,10 +29,22 @@ public class DimensionTpBlock extends Block {
 	public ActionResultType use(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
 		if (!worldIn.isClientSide() && player instanceof ServerPlayerEntity) {
 			ServerPlayerEntity serverPlayer = (ServerPlayerEntity) player;
-			RegistryKey<World> key = RegistryKey.create(Registry.DIMENSION_REGISTRY, DMADimensions.ANDROZANIMINOR.location());
+			RegistryKey<World> key = worldIn.dimension() == DMADimensions.ANDROZANIMINOR ? World.OVERWORLD : DMADimensions.ANDROZANIMINOR;
 			ServerWorld destinationWorld = worldIn.getServer().getLevel(key);
 			if (destinationWorld != null && destinationWorld != worldIn) {
-				serverPlayer.teleportTo(destinationWorld, player.getX(), player.getY(), player.getZ(), serverPlayer.getYHeadRot(), player.xRot);
+				BlockPos headPos = player.blockPosition().above();
+				double y = player.getY();
+				if (destinationWorld.getBlockState(headPos).isSuffocating(worldIn, pos)) {
+					for (int i = -1; i <= 1; i++) {
+						for (int j = -1; j <= 1; j++) {
+							for (int l = -1; l <= 1; l++) {
+								BlockPos bPos = pos.offset(i, j, l);
+								destinationWorld.setBlockAndUpdate(bPos, Blocks.AIR.defaultBlockState());
+							}
+						}
+					}
+				} serverPlayer.teleportTo(destinationWorld, player.getX(), player.getY(), player.getZ(), serverPlayer.getYHeadRot(), player.xRot);
+				destinationWorld.setBlockAndUpdate(pos, DMABlocks.CHRISTMAS_PRESENT.get().defaultBlockState());
 			}
 		}
 		return ActionResultType.SUCCESS;
