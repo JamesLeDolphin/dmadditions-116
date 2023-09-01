502820e8c99700e496fd3a4bddfe67df2f2bcac9
Torchwood-1 <jacobroberts@virginmedia.com>
2023-09-01 20:23:11 +0100

    added a model and renderer for beatrice

---
+++ src/main/java/com/jdolphin/dmadditions/client/render/entity/BetriceFlyingSharkRenderer.java
@@ -0,0 +1,21 @@
+package com.jdolphin.dmadditions.client.render.entity;
+
+
+import com.jdolphin.dmadditions.client.model.entity.BeatriceFlyingSharkModel;
+
+import com.jdolphin.dmadditions.entity.BeatriceFlyingSharkEntity;
+import net.minecraft.client.renderer.entity.EntityRendererManager;
+import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.util.ResourceLocation;
+
+public class BetriceFlyingSharkRenderer extends MobRenderer<BeatriceFlyingSharkEntity, BeatriceFlyingSharkModel> {
+
+	public BetriceFlyingSharkRenderer(EntityRendererManager p_i46179_1_) {
+		super(p_i46179_1_, new BeatriceFlyingSharkModel(), 1.0f);
+	}
+
+	@Override
+	public ResourceLocation getTextureLocation(BeatriceFlyingSharkEntity p_110775_1_) {
+		return this.model.model.getModelData().getTexture();
+	}
+}
