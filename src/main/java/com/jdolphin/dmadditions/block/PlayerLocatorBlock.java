6ad9c925aefca4705e1fa1b765d679b85df08f58
JamesLeDolphin <karl.kallip@gmail.com>
2023-09-09 19:09:51 +0300

    made PlayerLocatorBlock class

--- src/main/java/com/jdolphin/dmadditions/block/PlayerLocatorBlock.java
+++ src/main/java/com/jdolphin/dmadditions/block/PlayerLocatorBlock.java
@@ -1,13 +1,25 @@
 package com.jdolphin.dmadditions.block;
 
+import com.swdteam.common.init.*;
+import com.swdteam.common.tardis.Tardis;
+import com.swdteam.common.tardis.TardisData;
+import com.swdteam.network.NetworkHandler;
+import com.swdteam.network.packets.PacketOpenGui;
+import com.swdteam.network.packets.PacketXPSync;
+import com.swdteam.util.ChatUtil;
 import jdk.nashorn.internal.ir.Statement;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.BlockItemUseContext;
-import net.minecraft.util.Direction;
-import net.minecraft.util.ResourceLocation;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
 import net.minecraftforge.common.IPlantable;
 import net.minecraftforge.common.ToolType;
 
@@ -19,9 +31,18 @@ public class PlayerLocatorBlock extends Block implements IBetterPanel {
 		super(properties);
 	}
 
+	public ActionResultType use(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
+		if (!worldIn.isClientSide && worldIn.dimension().equals(DMDimensions.TARDIS)) {
+			return ActionResultType.CONSUME;
+		} else {
+			return ActionResultType.CONSUME;
+		}
+	}
+
+
 	@Override
 	public BlockState getStateForPlacement(BlockItemUseContext context) {
-		return null;
+		return IBetterPanel.super.getStateForPlacement(context, defaultBlockState());
 	}
 
 	@Override
@@ -31,7 +52,7 @@ public class PlayerLocatorBlock extends Block implements IBetterPanel {
 
 	@Override
 	public ToolType getHarvestTool(BlockState state) {
-		return null;
+		return ToolType.PICKAXE;
 	}
 
 	@Override
