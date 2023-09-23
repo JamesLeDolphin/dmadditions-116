17111ecd8274c26400b13af9c7fc902ab6bf8de8
JamesLeDolphin <karl.kallip@gmail.com>
2023-09-23 18:47:19 +0300

    improve DMAEntities

--- src/main/java/com/jdolphin/dmadditions/init/DMAEntities.java
+++ src/main/java/com/jdolphin/dmadditions/init/DMAEntities.java
@@ -10,42 +10,33 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.RegistryObject;
 
 public class DMAEntities {
-	public static RegistryObject<EntityType<WoodenCybermanEntity>> WOODEN_CYBERMAN;
-	public static RegistryObject<EntityType<PilotFishEntity>> PILOT_FISH;
-	public static RegistryObject<EntityType<SnowmanEntity>> SNOWMAN;
-	public static RegistryObject<EntityType<ChristmasTreeEntity>> CHRISTMAS_TREE;
-	public static RegistryObject<EntityType<BessieEntity>> BESSIE;
 
-	public static final RegistryObject<EntityType<JamesLeDolphinEntity>> JAMESLEDOLPHIN;
-	public static RegistryObject<EntityType<TorchwoodSuvEntity>> TW_SUV;
-
-	static {
-		JAMESLEDOLPHIN = DMARegistries.ENTITY_TYPES.register("jamesledolphin",
+	public static final RegistryObject<EntityType<JamesLeDolphinEntity>> JAMESLEDOLPHIN = DMARegistries.ENTITY_TYPES.register("jamesledolphin",
 			() -> EntityType.Builder.of(JamesLeDolphinEntity::new, EntityClassification.WATER_CREATURE).sized(0.9F, 0.6F)
 				.build((new ResourceLocation(DmAdditions.MODID, "jamesledolphin")).toString()));
 
-		PILOT_FISH = DMARegistries.ENTITY_TYPES.register("pilot_fish",
+	public static RegistryObject<EntityType<PilotFishEntity>> PILOT_FISH = DMARegistries.ENTITY_TYPES.register("pilot_fish",
 			() -> EntityType.Builder.of(PilotFishEntity::new, EntityClassification.MONSTER).sized(0.6f, 1.9f)
 				.build((new ResourceLocation(DalekMod.MODID, "pilot_fish")).toString()));
 
-		SNOWMAN = DMARegistries.ENTITY_TYPES.register("snowman",
+	public static RegistryObject<EntityType<SnowmanEntity>> SNOWMAN = DMARegistries.ENTITY_TYPES.register("snowman",
 			() -> EntityType.Builder.of(SnowmanEntity::new, EntityClassification.MONSTER)
 				.build((new ResourceLocation(DalekMod.MODID, "snowman")).toString()));
 
-		CHRISTMAS_TREE = DMARegistries.ENTITY_TYPES.register("christmas_tree",
+	public static RegistryObject<EntityType<ChristmasTreeEntity>> CHRISTMAS_TREE = DMARegistries.ENTITY_TYPES.register("christmas_tree",
 				() -> EntityType.Builder.of(ChristmasTreeEntity::new, EntityClassification.MONSTER)
 					.build((new ResourceLocation(DalekMod.MODID, "christmas_tree")).toString()));
 
-		WOODEN_CYBERMAN = DMARegistries.ENTITY_TYPES.register("wooden_cyberman",
+	public static RegistryObject<EntityType<WoodenCybermanEntity>> WOODEN_CYBERMAN = DMARegistries.ENTITY_TYPES.register("wooden_cyberman",
 				() -> EntityType.Builder.of(WoodenCybermanEntity::new, EntityClassification.MONSTER).sized(0.6F, 1.9F)
 					.build((new ResourceLocation(DalekMod.MODID, "wooden_cyberman")).toString()));
 
-		BESSIE = DMARegistries.ENTITY_TYPES.register("bessie",
+	public static RegistryObject<EntityType<BessieEntity>> BESSIE = DMARegistries.ENTITY_TYPES.register("bessie",
 				() -> EntityType.Builder.of(BessieEntity::new, EntityClassification.MISC).sized(0.9F, 0.6F)
 					.build((new ResourceLocation(DmAdditions.MODID, "bessie")).toString()));
 
-			TW_SUV = DMARegistries.ENTITY_TYPES.register("torchwood_suv",
+	public static RegistryObject<EntityType<TorchwoodSuvEntity>> TW_SUV = DMARegistries.ENTITY_TYPES.register("torchwood_suv",
 				() -> EntityType.Builder.of(TorchwoodSuvEntity::new, EntityClassification.MISC).sized(3F, 2F)
 					.build((new ResourceLocation(DmAdditions.MODID, "torchwood_suv")).toString()));
-	}
+
 }
