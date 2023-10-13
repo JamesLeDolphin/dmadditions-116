e569f5b9d66cc4ebcb3f27a77a2698a51fdb827a
Sam Tees <26529248+SamTees@users.noreply.github.com>
2023-10-13 20:31:55 +0100

    Make baubles only placeable under blocks

--- src/main/java/com/jdolphin/dmadditions/block/BaubleBlock.java
+++ src/main/java/com/jdolphin/dmadditions/block/BaubleBlock.java
@@ -3,11 +3,16 @@ package com.jdolphin.dmadditions.block;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.material.Material;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.IWorldReader;
 
 public class BaubleBlock extends Block {
 	public BaubleBlock(Properties p_i48440_1_) {
@@ -16,6 +21,17 @@ public class BaubleBlock extends Block {
 
 	public BaubleBlock(){
 		this(AbstractBlock.Properties.of(Material.GLASS).noOcclusion());
+	}
+
+	public BlockState updateShape(BlockState blockState, Direction direction, BlockState blockState1, IWorld world, BlockPos pos, BlockPos pos1) {
+		return !this.canSurvive(blockState, world, pos) ? Blocks.AIR.defaultBlockState() : super.updateShape(blockState, direction, blockState1, world, pos, pos1);
+	}
+
+	public boolean canSurvive(BlockState blockState, IWorldReader reader, BlockPos pos) {
+		BlockPos abovePos = pos.above();
+		BlockState aboveState = reader.getBlockState(abovePos);
+
+		return aboveState.isFaceSturdy(reader, abovePos, Direction.DOWN) || aboveState.is(BlockTags.LEAVES);
 	}
 
 	@Override
