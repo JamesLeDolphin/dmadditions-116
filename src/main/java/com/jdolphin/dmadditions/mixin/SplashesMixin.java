a474a91151a6ae855fe084248cba33c1e0e33569
Sam Tees <26529248+SamTees@users.noreply.github.com>
2023-11-27 00:21:04 +0000

    add splashes

---
+++ src/main/java/com/jdolphin/dmadditions/mixin/SplashesMixin.java
@@ -0,0 +1,66 @@
+package com.jdolphin.dmadditions.mixin;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+import org.apache.logging.log4j.LogManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.ibm.icu.util.Calendar;
+import com.jdolphin.dmadditions.advent.AdventUnlock;
+import com.swdteam.client.data.Splashes;
+
+@Mixin(Splashes.class)
+public class SplashesMixin {
+	@Inject(method = "load", at = @At("TAIL"), remap = false)
+	private static void load(CallbackInfo ci) {
+
+		LogManager.getLogger().info("Hello world from SplashesMixin");
+
+		ArrayList<String> splashes = new ArrayList<String>(Arrays.asList(Splashes.SPLASHES)) {
+			{
+				add("I blame James");
+				add("Bloody Torchwood!");
+
+				add("I'll ask bug");
+				add("I'll ask json");
+
+				add("The secret is our public open-source technology!");
+
+				add("Sam secretly did all the work");
+				add(":3");
+
+				add("Not to be confused with DMSubtractions!");
+				add("Also try Wicked!");
+
+				add("java.io.FileNotFoundException");
+				add("java.lang.NullPointerException");
+
+				add("Soon™");
+			}
+		};
+
+		if (AdventUnlock.isDecember()) {
+			splashes.add("DM Advent Calandar!?");
+		}
+
+		if (AdventUnlock.getCalendar().get(Calendar.MONTH) == Calendar.APRIL && AdventUnlock.getDate() == 1) {
+			Random random = new Random();
+
+			if (random.nextBoolean()) {
+				splashes.replaceAll(string -> new StringBuilder(string).reverse().toString());
+			}
+
+			if (random.nextFloat() < 0.02) {
+				splashes.replaceAll(string -> "§k" + string);
+			}
+		}
+
+		Splashes.SPLASHES = splashes.stream().toArray(String[]::new);
+
+	}
+}
