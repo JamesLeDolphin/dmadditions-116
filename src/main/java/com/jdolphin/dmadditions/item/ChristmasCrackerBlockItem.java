58898ec42f988eee4d538807e00da37d0c8eb935
Sam Tees <26529248+SamTees@users.noreply.github.com>
2023-12-08 01:10:34 +0000

    make cracker use lang for tooltip

--- src/main/java/com/jdolphin/dmadditions/item/ChristmasCrackerBlockItem.java
+++ src/main/java/com/jdolphin/dmadditions/item/ChristmasCrackerBlockItem.java
@@ -1,26 +1,25 @@
 package com.jdolphin.dmadditions.item;
 
+import java.util.List;
+
+import org.jetbrains.annotations.Nullable;
+
 import com.jdolphin.dmadditions.block.christmas.ChristmasCrackerBlock;
-import com.swdteam.common.init.DMNBTKeys;
-import com.swdteam.util.ItemUtils;
+
 import net.minecraft.block.Block;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.*;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
-import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.TextComponent;
-import net.minecraft.util.text.TextComponentUtils;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.RegistryObject;
-import org.jetbrains.annotations.Nullable;
-import slimeknights.mantle.registration.ItemProperties;
-
-import java.util.List;
 
 public class ChristmasCrackerBlockItem extends BlockItem {
 
@@ -45,6 +44,6 @@ public class ChristmasCrackerBlockItem extends BlockItem {
 	@Override
 	public void appendHoverText(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
 		super.appendHoverText(stack, worldIn, tooltip, flagIn);
-		ItemUtils.addText(tooltip, "Open to reveal a surprise!", TextFormatting.GRAY);
+		tooltip.add(new TranslationTextComponent("tooltip.item.dmadditions.christmas_cracker").withStyle(TextFormatting.GRAY));
 	}
 }
