d23e0ffe35ae809f98fb39c7b5f0a5d64e761183
JamesLeDolphin <karl.kallip@gmail.com>
2024-01-08 13:59:03 +0200

    mess w stuff idk

--- src/main/java/com/jdolphin/dmadditions/mixin/BotiMixin.java
+++ src/main/java/com/jdolphin/dmadditions/mixin/BotiMixin.java
@@ -6,18 +6,23 @@ import com.qouteall.immersive_portals.portal.PortalManipulation;
 import com.swdteam.client.tardis.data.ExteriorModels;
 import com.swdteam.common.init.DMBlockEntities;
 import com.swdteam.common.init.DMDimensions;
+import com.swdteam.common.init.DMFlightMode;
 import com.swdteam.common.init.DMTardis;
 import com.swdteam.common.tardis.*;
+import com.swdteam.common.teleport.TeleportRequest;
 import com.swdteam.common.tileentity.ExtraRotationTileEntityBase;
 import com.swdteam.common.tileentity.TardisTileEntity;
 import com.swdteam.model.javajson.JSONModel;
 import com.swdteam.model.javajson.ModelRendererWrapper;
 import com.swdteam.model.javajson.ModelWrapper;
 import com.swdteam.util.SWDMathUtils;
+import com.swdteam.util.TeleportUtil;
 import com.swdteam.util.math.Position;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.model.ModelRenderer;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.TileEntityType;
 import net.minecraft.util.Direction;
@@ -42,19 +47,22 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Iterator;
+import java.util.List;
 import java.util.Objects;
+import java.util.function.Predicate;
 
 import static com.swdteam.common.init.DMDimensions.TARDIS;
 
 @Mixin(TardisTileEntity.class)
 public abstract class BotiMixin extends ExtraRotationTileEntityBase implements ITickableTileEntity  {
 
+	@Shadow
+	public TardisData tardisData;
 	@Shadow boolean demat;
 	boolean spawned = true;
 	boolean removed = true;
 	boolean canRemove = false;
 
-	public Portal intPortal;
 	public Portal portal = null;
 
 	boolean isPortalSpawned = false;
@@ -70,17 +78,12 @@ public abstract class BotiMixin extends ExtraRotationTileEntityBase implements I
 
 	@Inject(method = "tick()V", at = @At("TAIL"), remap = false)
 	public void tick(CallbackInfo ci) {
-
 		TardisTileEntity t = (TardisTileEntity) ((Object) this);
-
-		if(portal != null)
-		{
-			portal.setId(8762);
+		if(portal != null) {
 		}
 
-		if (!this.level.isClientSide) {
+		if (!this.level.isClientSide()) {
 			t.tardisData = DMTardis.getTardis(t.globalID);
-
                     /*
                     TODO
                     Change portal size based on exterior
@@ -89,8 +92,6 @@ public abstract class BotiMixin extends ExtraRotationTileEntityBase implements I
                     //The portal still bugs and doesn't update the position sometimes
                      */
 			if (t.tardisData != null) {
-
-
 				if (t.tardisData.getInteriorSpawnPosition() != null) {
 
 					ResourceLocation rl = t.tardisData.getTardisExterior().getData().getModel(t.tardisData.getSkinID());
@@ -99,21 +100,24 @@ public abstract class BotiMixin extends ExtraRotationTileEntityBase implements I
 					ModelRendererWrapper mdl = modelWrapper.getPart("portal");
 
 					Position vec = t.tardisData.getInteriorSpawnPosition();
-					Vector3d pos = new Vector3d(vec.x(), vec.y() + 1, vec.z());
+					Vector3d pos = new Vector3d(vec.x(), vec.y() + 1.2, vec.z());
 					defaultAABB = new AxisAlignedBB(0.0, 0.0, 0.0, 1.0, 2.0, 1.0);
 					//size of the portal
-					AxisAlignedBB bounds = defaultAABB.move(this.getBlockPos()).inflate(-0.14200001192092896, 0.0, 0);
+					AxisAlignedBB bounds = defaultAABB.move(this.getBlockPos()).inflate(-0.14200001192092896, 0.0, -0.14200001192092896);
 
 					//handles how far out the portal is from the tardis
 					bounds = bounds.move(Math.sin(Math.toRadians((double) this.rotation)) * 0.1, 0.0, -Math.cos(Math.toRadians((double) this.rotation)) * 0.1);
-
 					Direction tDir = Direction.byName(SWDMathUtils.rotationToCardinal(t.rotation));
 
 					if (!t.doorOpenLeft && !t.doorOpenRight) {
 						//System.out.println(SWDMathUtils.rotationToCardinal(flightData.getRotationAngle()));
 						if (isPortalSpawned && portal != null && portal.isAlive()) {
+							portal.kill();
+							level.getChunk(portal.xChunk, portal.zChunk).removeEntity(portal);
 							portal.remove();
 							portal = null;
+							portal.tick();
+							PortalManipulation.removeConnectedPortals(portal, Portal::remove);
 							isPortalSpawned = false;
 						}
 					}
@@ -130,7 +134,7 @@ public abstract class BotiMixin extends ExtraRotationTileEntityBase implements I
 					 * dalekmod:sidrat_capsule
 					 */
 
-					if((t.doorOpenLeft || t.doorOpenRight) && !isPortalSpawned && tDir != null) {
+					if ((t.doorOpenLeft || t.doorOpenRight) && !isPortalSpawned && tDir != null) {
 						portal = PortalManipulation.createOrthodoxPortal(
 							Portal.entityType,
 							McHelper.getServerWorld(t.tardisData.getCurrentLocation().dimensionWorldKey()),
@@ -140,6 +144,7 @@ public abstract class BotiMixin extends ExtraRotationTileEntityBase implements I
 							pos
 						);
 						portal.setYBodyRot(t.getRotation());
+						portal.tick();
 						//portal.renderingMergable = true; //Recommended to check if overlapping portals, so doesn't have visual bugs
 						if(tDir == Direction.NORTH)
 						{
@@ -168,10 +173,25 @@ public abstract class BotiMixin extends ExtraRotationTileEntityBase implements I
 						}
 
 					}
+					List<Entity> entities = this.level.getEntitiesOfClass(Entity.class, bounds);
+					Predicate<Entity> inFlight = (entity) -> entity instanceof PlayerEntity && DMFlightMode.isInFlight((PlayerEntity)entity);
+					Predicate<Entity> isRiding = Entity::isPassenger;
+					entities.removeIf(inFlight);
+					entities.removeIf(isRiding);
+					entities.removeIf(e -> e instanceof Portal);
+					entities.remove(portal);
+					if (!entities.isEmpty()) {
+						Entity e = (Entity)entities.get(0);
+						Position vec1 = this.tardisData.getInteriorSpawnPosition();
+						if (!TeleportUtil.TELEPORT_REQUESTS.containsKey(e) && vec1 != null) {
+							Location loc = new Location(new Vector3d(vec1.x(), vec1.y(), vec1.z()), DMDimensions.TARDIS);
+							loc.setFacing(this.tardisData.getInteriorSpawnRotation() + e.getYHeadRot() - this.rotation);
+							TeleportUtil.TELEPORT_REQUESTS.put(e, new TeleportRequest(loc));
+						}
+					}
 				}
 
-				return;
-			}
+            }
 
 		}
 	}
