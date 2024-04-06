2baceaf8891e1d44406c9c858fdf89350cd77ebe
JamesLeDolphin <karl.kallip@gmail.com>
2024-04-06 20:42:14 +0300

    model, tooltip for fob watch

--- src/main/java/com/jdolphin/dmadditions/item/FobWatchItem.java
+++ src/main/java/com/jdolphin/dmadditions/item/FobWatchItem.java
@@ -3,6 +3,7 @@ package com.jdolphin.dmadditions.item;
 import com.jdolphin.dmadditions.init.DMACapabilities;
 import com.jdolphin.dmadditions.util.Helper;
 import com.swdteam.util.ChatUtil;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.Item;
@@ -10,9 +11,17 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
+import javax.annotation.Nullable;
 import java.awt.*;
+import java.util.List;
 
 public class FobWatchItem extends Item {
 
@@ -51,6 +60,11 @@ public class FobWatchItem extends Item {
 
 	public boolean showDurabilityBar(ItemStack stack) {
 		return true;
+	}
+
+	@OnlyIn(Dist.CLIENT)
+	public void appendHoverText(ItemStack stack, @Nullable World world, List<ITextComponent> textComponents, ITooltipFlag flag) {
+		textComponents.add(new StringTextComponent(stack.getMaxDamage() - stack.getDamageValue() + " regenerations").withStyle(TextFormatting.DARK_GRAY));
 	}
 
 
