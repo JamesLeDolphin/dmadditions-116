02952bd04b6e0bf4771900b6a591b77ef0432bcd
Torchwood-1 <jacobroberts@virginmedia.com>
2024-10-16 15:45:27 +0100

    I don't have a fucking clue what i'm doing, fuck my life, I don't understand anything i've just looked at, like what the fuck, how am i meant to put it on the players head, use a keybind and use the sonic menus as well ;(

---
+++ src/main/java/com/jdolphin/dmadditions/item/SonicShadesItem.java
@@ -0,0 +1,10 @@
+package com.jdolphin.dmadditions.item;
+
+import com.swdteam.common.item.sonics.SonicScrewdriverItem;
+import net.minecraft.item.ItemGroup;
+
+public class SonicShadesItem extends SonicScrewdriverItem {
+	public SonicShadesItem(ItemGroup itemGroup, int xpValue) {
+		super(itemGroup, xpValue);
+	}
+}
