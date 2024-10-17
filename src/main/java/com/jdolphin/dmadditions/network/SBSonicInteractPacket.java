c3f25cc5aac56a32f47e2af50dc4cdb8631230e3
JamesLeDolphin <karl.kallip@gmail.com>
2024-10-17 13:55:47 +0300

    sonic xp and energy

--- src/main/java/com/jdolphin/dmadditions/network/SBSonicInteractPacket.java
+++ src/main/java/com/jdolphin/dmadditions/network/SBSonicInteractPacket.java
@@ -5,20 +5,26 @@ import com.jdolphin.dmadditions.item.SonicShadesItem;
 import com.swdteam.common.init.DMSonicRegistry;
 import com.swdteam.common.init.DMSoundEvents;
 import com.swdteam.common.sonic.ISonicEntityInteraction;
+import com.swdteam.common.sonic.SonicCategory;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.ai.controller.LookController;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.EntityRayTraceResult;
 import net.minecraft.util.math.RayTraceContext;
 import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.common.extensions.IForgeEntity;
 import net.minecraftforge.fml.network.NetworkEvent;
 
 import java.util.function.Supplier;
@@ -34,39 +40,63 @@ public class SBSonicInteractPacket {
 	public void encode(PacketBuffer buf) {
 	}
 
+	private void sonicUse(CompoundNBT tag) {
+		tag.putInt("xp", tag.getInt("xp") + 1);
+		tag.putInt("energy", tag.getInt("energy") - 1);
+	}
+
 	public boolean handle(Supplier<NetworkEvent.Context> supplier) {
 		NetworkEvent.Context context = supplier.get();
 		ServerPlayerEntity player = context.getSender();
 
-		assert  player != null;
+		assert player != null;
 
 		try {
 			ServerWorld world = player.getLevel();
 			ItemStack stack = player.getItemBySlot(EquipmentSlotType.HEAD);
 			if (DMAItems.SONIC_SHADES != null && stack.getItem().equals(DMAItems.SONIC_SHADES.get())) {
 				SonicShadesItem.checkIsSetup(stack);
+
 				world.playSound(null, player.blockPosition(), DMSoundEvents.ENTITY_SONIC_ELEVENTH_EXTEND.get(), SoundCategory.PLAYERS, 0.5F, 1.0F);
 
 				RayTraceResult rayTraceResult = SonicShadesItem.getPlayerRayTraceResult(world, player, RayTraceContext.FluidMode.ANY);
-				System.out.println(rayTraceResult.getType());
+				CompoundNBT tag = stack.getOrCreateTag();
 				if (rayTraceResult.getType() == RayTraceResult.Type.BLOCK) {
 					BlockRayTraceResult result = (BlockRayTraceResult) rayTraceResult;
 					BlockState state = world.getBlockState(result.getBlockPos());
+
 					if (DMSonicRegistry.SONIC_LOOKUP.containsKey(state.getBlock())) {
 						DMSonicRegistry.ISonicInteraction sonic = DMSonicRegistry.SONIC_LOOKUP.get(state.getBlock());
-						sonic.interact(world, player, stack, result.getBlockPos());
+						if (sonic != null && tag.getInt("energy") > 0) {
+							if (!SonicCategory.canExecute(stack, sonic.getCategory()) && !player.isCreative()) {
+								player.displayClientMessage((new StringTextComponent("This ability is still locked")).withStyle(TextFormatting.RED), false);
+							} else {
+								sonic.interact(world, player, stack, result.getBlockPos());
+								sonicUse(tag);
+								SonicCategory.checkUnlock(player, stack);
+							}
+						}
 					}
 				}
-				if (rayTraceResult.getType() == RayTraceResult.Type.ENTITY) {
+				if (rayTraceResult.getType() == RayTraceResult.Type.MISS) {
 					EntityRayTraceResult result = (EntityRayTraceResult) rayTraceResult;
 					Entity entity = result.getEntity();
-					System.out.println(entity);
-					if (DMSonicRegistry.SONIC_LOOKUP.containsKey(entity)) {
-						DMSonicRegistry.ISonicInteraction sonic = DMSonicRegistry.SONIC_LOOKUP.get(entity.getType());
-						sonic.interact(world, player, stack, entity);
+					if (entity != null) {
+						System.out.println(entity);
+						if (DMSonicRegistry.SONIC_LOOKUP.containsKey(entity)) {
+							DMSonicRegistry.ISonicInteraction sonic = DMSonicRegistry.SONIC_LOOKUP.get(entity.getType());
+							if (sonic != null && tag.getInt("energy") > 0) {
+								if (!SonicCategory.canExecute(stack, sonic.getCategory()) && !player.isCreative()) {
+									player.displayClientMessage((new StringTextComponent("This ability is still locked")).withStyle(TextFormatting.RED), false);
+								} else {
+									sonic.interact(world, player, stack, entity);
+									sonicUse(tag);
+									SonicCategory.checkUnlock(player, stack);
+								}
+							}
+						}
 					}
 				}
-
 
 				return true;
 			}
