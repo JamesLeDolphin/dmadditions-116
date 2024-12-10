bd117b8bdf80ef0816e84136c0089733dda75570
JamesLeDolphin <68230871+JamesLeDolphin@users.noreply.github.com>
2024-12-10 08:48:39 +0200

    Fix incorrect capitalisation

--- src/main/java/com/jdolphin/dmadditions/init/DMAFluids.java
+++ src/main/java/com/jdolphin/dmadditions/init/DMAFluids.java
@@ -15,9 +15,9 @@ public class DMAFluids {
 
 	public static final FluidObject<ForgeFlowingFluid> MOLTEN_DALEKANIUM = FLUIDS.register("molten_dalekanium", hotBuilder().temperature(1234), Material.LAVA, 9);
 	public static final FluidObject<ForgeFlowingFluid> MOLTEN_STEEL = FLUIDS.register("molten_steel", hotBuilder().temperature(1234), Material.LAVA, 9);
-	public static final FluidObject<ForgeFlowingFluid> MOLTEN_STAINLESS_STEEL = FLUIDS.register("MOLTEN_STAINLESS_STEEL", hotBuilder().temperature(1234), Material.LAVA, 9);
-	public static final FluidObject<ForgeFlowingFluid> MOLTEN_METALERT = FLUIDS.register("MOLTEN_METALERT", hotBuilder().temperature(1234), Material.LAVA, 9);
-	public static final FluidObject<ForgeFlowingFluid> MOLTEN_SILICON = FLUIDS.register("MOLTEN_SILICON", hotBuilder().temperature(1234), Material.LAVA, 9);
+	public static final FluidObject<ForgeFlowingFluid> MOLTEN_STAINLESS_STEEL = FLUIDS.register("molten_stainless_steel", hotBuilder().temperature(1234), Material.LAVA, 9);
+	public static final FluidObject<ForgeFlowingFluid> MOLTEN_METALERT = FLUIDS.register("molten_metalert", hotBuilder().temperature(1234), Material.LAVA, 9);
+	public static final FluidObject<ForgeFlowingFluid> MOLTEN_SILICON = FLUIDS.register("molten_silicon", hotBuilder().temperature(1234), Material.LAVA, 9);
 
 
 	private static FluidAttributes.Builder hotBuilder() {
