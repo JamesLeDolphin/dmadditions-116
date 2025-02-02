bf8bd5f120d29957b021206e7711c0074262c108
JamesLeDolphin <karl.kallip@gmail.com>
2025-02-02 19:31:50 +0200

    citadel modifications im not sure work well but eh

--- src/main/java/com/jdolphin/dmadditions/world/structure/CitadelStructure.java
+++ src/main/java/com/jdolphin/dmadditions/world/structure/CitadelStructure.java
@@ -7,18 +7,21 @@ import com.jdolphin.dmadditions.init.DMAEntities;
 import com.mojang.serialization.Codec;
 import com.swdteam.util.world.Schematic;
 import com.swdteam.util.world.SchematicUtils;
+import net.minecraft.util.SharedSeedRandom;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.MutableBoundingBox;
 import net.minecraft.util.registry.DynamicRegistries;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.MobSpawnInfo;
+import net.minecraft.world.biome.provider.BiomeProvider;
 import net.minecraft.world.gen.ChunkGenerator;
 import net.minecraft.world.gen.GenerationStage.Decoration;
 import net.minecraft.world.gen.feature.NoFeatureConfig;
 import net.minecraft.world.gen.feature.structure.Structure;
 import net.minecraft.world.gen.feature.structure.StructureStart;
 import net.minecraft.world.gen.feature.template.TemplateManager;
+import net.minecraft.world.gen.settings.StructureSeparationSettings;
 import net.minecraftforge.fml.server.ServerLifecycleHooks;
 
 import javax.annotation.ParametersAreNonnullByDefault;
@@ -50,6 +53,27 @@ public class CitadelStructure extends Structure<NoFeatureConfig> {
 		return STRUCTURE_CREATURES;
 	}
 
+	public StructureStart<?> generate(DynamicRegistries registries, ChunkGenerator generator, BiomeProvider biomeProvider,
+									  TemplateManager manager, long seed, ChunkPos chunkPos, Biome biome, int i, SharedSeedRandom seedRandom,
+									  StructureSeparationSettings separationSettings, NoFeatureConfig featureConfig) {
+
+		if (DMACommonConfig.generateCitadel()) {
+			BlockPos pos1 = new BlockPos(-4320, 0, -4800);
+			BlockPos pos = chunkPos.getWorldPosition();
+			if (Objects.requireNonNull(biome.getRegistryName()).toString().contains("dmadditions:gallifrey")) {
+				if (pos.equals(pos1)) {
+					Schematic schematic = SchematicUtils.loadSchematic("citadel", SchematicUtils.FileLocation.INTERNAL);
+					SchematicUtils.generateSchematic(SchematicUtils.GenerationQueue.CITADEL,
+						ServerLifecycleHooks.getCurrentServer().getLevel(DMADimensions.GALLIFREY), pos, schematic);
+				}
+			}
+		}
+
+		return new Start(this, chunkPos.x, chunkPos.z,
+			MutableBoundingBox.createProper(0, 0, 0, 410, 201, 205),
+			i, seed);
+	}
+
 	public static class Start extends StructureStart<NoFeatureConfig> {
 		public Start(Structure<NoFeatureConfig> structureIn, int chunkX, int chunkZ, MutableBoundingBox mutableBoundingBox, int referenceIn, long seedIn) {
 			super(structureIn, chunkX, chunkZ, mutableBoundingBox, referenceIn, seedIn);
@@ -57,19 +81,7 @@ public class CitadelStructure extends Structure<NoFeatureConfig> {
 
 		@ParametersAreNonnullByDefault
 		public void generatePieces(DynamicRegistries registries, ChunkGenerator generator, TemplateManager manager, int chunkX, int chunkZ, Biome biomeIn, NoFeatureConfig config) {
-			if (DMACommonConfig.generateCitadel()) {
-				this.boundingBox = MutableBoundingBox.createProper(0, 0, 0, 410, 201, 205);
-				BlockPos pos1 = new BlockPos(-4320, 0, -4800);
-				ChunkPos chunkPos = new ChunkPos(chunkX, chunkZ);
-				BlockPos pos = chunkPos.getWorldPosition();
-				if (Objects.requireNonNull(biomeIn.getRegistryName()).toString().contains("dmadditions:gallifrey")) {
-					if (pos.equals(pos1)) {
-						Schematic schematic = SchematicUtils.loadSchematic("citadel", SchematicUtils.FileLocation.INTERNAL);
-						SchematicUtils.generateSchematic(SchematicUtils.GenerationQueue.CITADEL,
-							ServerLifecycleHooks.getCurrentServer().getLevel(DMADimensions.GALLIFREY), pos, schematic);
-					}
-				}
-			}
+
 		}
 	}
 }
